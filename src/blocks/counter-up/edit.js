3c594564c3d4cc69ff72de11d78ac197d629c0eb
Asaquzzaman <joy.mishu@gmail.com>
2020-12-16 19:34:20 +0600

    Set typography for number and title

--- src/blocks/counter-up/edit.js
+++ src/blocks/counter-up/edit.js
@@ -6,6 +6,7 @@
  * Internal dependencies
  */
 import Inspector from './inspector';
+import KadenceColorOutput from '../../kadence-color-output';
 
 /**
  * Import External
@@ -70,6 +71,7 @@ class KadenceCounterUp extends Component {
 		} = this.props;
 
 		const {
+			uniqueID,
 			title,
 			start,
 			end,
@@ -77,27 +79,102 @@ class KadenceCounterUp extends Component {
 			suffix,
 			duration,
 			separator,
-			uniqueID
+			displayTitle,
+			titleFont,
+			titleColor,
+			titleHoverColor,
+			titleMinHeight,
+			numberFont,
+			numberColor,
+			numberHoverColor,
+			numberMinHeight,
 		} = attributes
+
+		const titleTagName = 'h' + titleFont[ 0 ].level;
+
+		const gconfig = {
+			google: {
+				families: [ titleFont[ 0 ].family + ( titleFont[ 0 ].variant ? ':' + titleFont[ 0 ].variant : '' ) ],
+			},
+		};
+		const config = ( titleFont[ 0 ].google ? gconfig : '' );
+		const ngconfig = {
+			google: {
+				families: [ numberFont[ 0 ].family + ( numberFont[ 0 ].variant ? ':' + numberFont[ 0 ].variant : '' ) ],
+			},
+		};
+		const nconfig = ( numberFont[ 0 ].google ? ngconfig : '' );
+
+		const renderCSS = (
+			<style>
+				{ ( titleHoverColor ? `#kt-counter-up-${ uniqueID }:hover .kt-counter-up-title { color: ${ KadenceColorOutput( titleHoverColor ) } !important; }` : '' ) }
+				{ ( numberHoverColor ? `#kt-counter-up-${ uniqueID }:hover .kt-counter-up-number { color: ${ KadenceColorOutput( numberHoverColor ) } !important; }` : '' ) }
+
+			</style>
+		);
 
 		return (
 			<Fragment>
 				{ isSelected && <Inspector {...this.props} /> }
-				<div className={ classnames( 'kt-counter-up' ) }>
-					<CountUp
-					  start={ start }
-					  end={ end }
-					  duration={ duration }
-					  separator={ separator ? ',' : '' }
-					  prefix={ prefix }
-					  suffix={ suffix }
-					/>
-					<RichText
-						tagName="div"
-						value={ title }
-						placeholder={ __( 'Type Here...' ) }
-						onChange={ (content) => setAttributes({ title: content }) }
-					/>
+
+				{ displayTitle && titleFont[ 0 ].google && (
+					<WebfontLoader config={ config } />
+				) }
+
+				{ numberFont[ 0 ].google && (
+					<WebfontLoader config={ nconfig } />
+				) }
+
+				{ renderCSS }
+
+				<div id={ `kt-counter-up-${uniqueID}` } className={ classnames( 'kt-counter-up' ) }>
+					<span
+						className={ classnames( 'kt-counter-up-number' ) }
+						style={ {
+							fontWeight: numberFont[ 0 ].weight,
+							fontStyle: numberFont[ 0 ].style,
+							display: 'inline-flex',
+							color: KadenceColorOutput( numberColor ),
+							fontSize: numberFont[ 0 ].size[ 0 ] + numberFont[ 0 ].sizeType,
+							lineHeight: ( numberFont[ 0 ].lineHeight && numberFont[ 0 ].lineHeight[ 0 ] ? numberFont[ 0 ].lineHeight[ 0 ] + numberFont[ 0 ].lineType : undefined ),
+							letterSpacing: numberFont[ 0 ].letterSpacing + 'px',
+							fontFamily: ( numberFont[ 0 ].family ? numberFont[ 0 ].family : '' ),
+							padding: ( numberFont[ 0 ].padding ? numberFont[ 0 ].padding[ 0 ] + 'px ' + numberFont[ 0 ].padding[ 1 ] + 'px ' + numberFont[ 0 ].padding[ 2 ] + 'px ' + numberFont[ 0 ].padding[ 3 ] + 'px' : '' ),
+							margin: ( numberFont[ 0 ].margin ? numberFont[ 0 ].margin[ 0 ] + 'px ' + numberFont[ 0 ].margin[ 1 ] + 'px ' + numberFont[ 0 ].margin[ 2 ] + 'px ' + numberFont[ 0 ].margin[ 3 ] + 'px' : '' ),
+							minHeight: ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 0 ] ? titleMinHeight[ 0 ] + 'px' : undefined ),
+						} }
+					>
+						<CountUp
+						  start={ start }
+						  end={ end }
+						  duration={ duration }
+						  separator={ separator ? ',' : '' }
+						  prefix={ prefix }
+						  suffix={ suffix }
+						/>
+					</span>
+
+					{ 	displayTitle &&
+						<RichText
+							tagName={ titleTagName }
+							className={ classnames( className, 'kt-counter-up-title' ) }
+							value={ title }
+							onChange={ (content) => setAttributes({ title: content }) }
+							placeholder={ __( 'Type Here...' ) }
+							style={ {
+								fontWeight: titleFont[ 0 ].weight,
+								fontStyle: titleFont[ 0 ].style,
+								color: KadenceColorOutput( titleColor ),
+								fontSize: titleFont[ 0 ].size[ 0 ] + titleFont[ 0 ].sizeType,
+								lineHeight: ( titleFont[ 0 ].lineHeight && titleFont[ 0 ].lineHeight[ 0 ] ? titleFont[ 0 ].lineHeight[ 0 ] + titleFont[ 0 ].lineType : undefined ),
+								letterSpacing: titleFont[ 0 ].letterSpacing + 'px',
+								fontFamily: ( titleFont[ 0 ].family ? titleFont[ 0 ].family : '' ),
+								padding: ( titleFont[ 0 ].padding ? titleFont[ 0 ].padding[ 0 ] + 'px ' + titleFont[ 0 ].padding[ 1 ] + 'px ' + titleFont[ 0 ].padding[ 2 ] + 'px ' + titleFont[ 0 ].padding[ 3 ] + 'px' : '' ),
+								margin: ( titleFont[ 0 ].margin ? titleFont[ 0 ].margin[ 0 ] + 'px ' + titleFont[ 0 ].margin[ 1 ] + 'px ' + titleFont[ 0 ].margin[ 2 ] + 'px ' + titleFont[ 0 ].margin[ 3 ] + 'px' : '' ),
+								minHeight: ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 0 ] ? titleMinHeight[ 0 ] + 'px' : undefined ),
+							} }
+						/>
+					}
 				</div>
 			</Fragment>
 		)
--- src/blocks/counter-up/inspector.js
+++ src/blocks/counter-up/inspector.js
@@ -10,6 +10,9 @@ import map from 'lodash/map';
 /**
  * Internal dependencies
  */
+import TypographyControls from '../../typography-control';
+import AdvancedPopColorControl from '../../advanced-pop-color-control';
+import KadenceRange from '../../kadence-range-control';
 
 /**
  * WordPress dependencies
@@ -58,15 +61,48 @@ class Inspector extends Component {
 			prefix,
 			suffix,
 			duration,
-			separator
+			separator,
+			displayTitle,
+			titleFont,
+			titleColor,
+			titleHoverColor,
+			titleMinHeight,
+			numberColor,
+			numberHoverColor,
+			numberMinHeight,
+			numberFont
 		} = attributes;
+
+		const saveTitleFont = ( value ) => {
+			const newUpdate = titleFont.map( ( item, index ) => {
+				if ( 0 === index ) {
+					item = { ...item, ...value };
+				}
+				return item;
+			} );
+			setAttributes( {
+				titleFont: newUpdate,
+			} );
+		};
+
+		const saveNumberFont = ( value ) => {
+			const newUpdate = numberFont.map( ( item, index ) => {
+				if ( 0 === index ) {
+					item = { ...item, ...value };
+				}
+				return item;
+			} );
+			setAttributes( {
+				numberFont: newUpdate,
+			} );
+		};
 
 		return (
 			<Fragment>
 				<InspectorControls>
 					<PanelBody
 						title={ __( 'Counter Up Settings' ) }
-						initialOpen={ false }>
+						initialOpen={ true }>
 
 						<div className="kt-columns-control">
 							<RangeControl
@@ -114,6 +150,320 @@ class Inspector extends Component {
 								onChange={ ( value ) => setAttributes( { separator: value } ) }
 							/>
 						</div>
+					</PanelBody>
+
+					<PanelBody
+						title={ __( 'Title Settings' ) }
+						initialOpen={ true }>
+						<ToggleControl
+							label={ __( 'Show Title' ) }
+							checked={ displayTitle }
+							onChange={ ( value ) => setAttributes( { displayTitle: value } ) }
+						/>
+						{
+							displayTitle &&
+							<Fragment>
+								<h2 className="kt-tab-wrap-title">{ __( 'Color Settings' ) }</h2>
+								<TabPanel className="kt-inspect-tabs kt-hover-tabs"
+									activeClass="active-tab"
+									tabs={ [
+										{
+											name: 'normal',
+											title: __( 'Normal' ),
+											className: 'kt-normal-tab',
+										},
+										{
+											name: 'hover',
+											title: __( 'Hover' ),
+											className: 'kt-hover-tab',
+										},
+									] }>
+									{
+										( tab ) => {
+											let tabout;
+											if ( tab.name ) {
+												if ( 'hover' === tab.name ) {
+													tabout = (
+														<AdvancedPopColorControl
+															label={ __( 'Hover Color' ) }
+															colorValue={ ( titleHoverColor ? titleHoverColor : '' ) }
+															colorDefault={ '' }
+															onColorChange={ value => setAttributes( { titleHoverColor: value } ) }
+														/>
+													);
+												} else {
+													tabout = (
+														<AdvancedPopColorControl
+															label={ __( 'Title Color' ) }
+															colorValue={ ( titleColor ? titleColor : '' ) }
+															colorDefault={ '' }
+															onColorChange={ value => setAttributes( { titleColor: value } ) }
+														/>
+													);
+												}
+											}
+											return <div className={ tab.className } key={ tab.className }>{ tabout }</div>;
+										}
+									}
+								</TabPanel>
+
+								<h2 className="kt-heading-size-title">{ __( 'Min Height' ) }</h2>
+								<TabPanel className="kt-size-tabs"
+									activeClass="active-tab"
+									tabs={ [
+										{
+											name: 'desk',
+											title: <Dashicon icon="desktop" />,
+											className: 'kt-desk-tab',
+										},
+										{
+											name: 'tablet',
+											title: <Dashicon icon="tablet" />,
+											className: 'kt-tablet-tab',
+										},
+										{
+											name: 'mobile',
+											title: <Dashicon icon="smartphone" />,
+											className: 'kt-mobile-tab',
+										},
+									] }>
+									{
+										( tab ) => {
+											let tabout;
+											if ( tab.name ) {
+												if ( 'mobile' === tab.name ) {
+													tabout = (
+														<KadenceRange
+															value={ ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 2 ] ) ? titleMinHeight[ 2 ] : '' ) }
+															onChange={ value => setAttributes( { titleMinHeight: [ ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 0 ] ) ? titleMinHeight[ 0 ] : '' ), ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 1 ] ) ? titleMinHeight[ 1 ] : '' ), value ] } ) }
+															step={ 1 }
+															min={ 0 }
+															max={ 600 }
+														/>
+													);
+												} else if ( 'tablet' === tab.name ) {
+													tabout = (
+														<KadenceRange
+															value={ ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 1 ] ) ? titleMinHeight[ 1 ] : '' ) }
+															onChange={ value => setAttributes( { titleMinHeight: [ ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 0 ] ) ? titleMinHeight[ 0 ] : '' ), value, ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 2 ] ) ? titleMinHeight[ 2 ] : '' ) ] } ) }
+															step={ 1 }
+															min={ 0 }
+															max={ 600 }
+														/>
+													);
+												} else {
+													tabout = (
+														<KadenceRange
+															value={ ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 0 ] ) ? titleMinHeight[ 0 ] : '' ) }
+															onChange={ value => setAttributes( { titleMinHeight: [ value, ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 1 ] ) ? titleMinHeight[ 1 ] : '' ), ( ( undefined !== titleMinHeight && undefined !== titleMinHeight[ 2 ] ) ? titleMinHeight[ 2 ] : '' ) ] } ) }
+															step={ 1 }
+															min={ 0 }
+															max={ 600 }
+														/>
+													);
+												}
+											}
+											return <div className={ tab.className } key={ tab.className }>{ tabout }</div>;
+										}
+									}
+								</TabPanel>
+								<TypographyControls
+								fontGroup={ 'heading' }
+								tagLevel={ titleFont[ 0 ].level }
+								onTagLevel={ ( value ) => saveTitleFont( { level: value } ) }
+								fontSize={ titleFont[ 0 ].size }
+								onFontSize={ ( value ) => saveTitleFont( { size: value } ) }
+								fontSizeType={ titleFont[ 0 ].sizeType }
+								onFontSizeType={ ( value ) => saveTitleFont( { sizeType: value } ) }
+								lineHeight={ titleFont[ 0 ].lineHeight }
+								onLineHeight={ ( value ) => saveTitleFont( { lineHeight: value } ) }
+								lineHeightType={ titleFont[ 0 ].lineType }
+								onLineHeightType={ ( value ) => saveTitleFont( { lineType: value } ) }
+								letterSpacing={ titleFont[ 0 ].letterSpacing }
+								onLetterSpacing={ ( value ) => saveTitleFont( { letterSpacing: value } ) }
+								fontFamily={ titleFont[ 0 ].family }
+								onFontFamily={ ( value ) => saveTitleFont( { family: value } ) }
+								onFontChange={ ( select ) => {
+									saveTitleFont( {
+										family: select.value,
+										google: select.google,
+									} );
+								} }
+								onFontArrayChange={ ( values ) => saveTitleFont( values ) }
+								googleFont={ titleFont[ 0 ].google }
+								onGoogleFont={ ( value ) => saveTitleFont( { google: value } ) }
+								loadGoogleFont={ titleFont[ 0 ].loadGoogle }
+								onLoadGoogleFont={ ( value ) => saveTitleFont( { loadGoogle: value } ) }
+								fontVariant={ titleFont[ 0 ].variant }
+								onFontVariant={ ( value ) => saveTitleFont( { variant: value } ) }
+								fontWeight={ titleFont[ 0 ].weight }
+								onFontWeight={ ( value ) => saveTitleFont( { weight: value } ) }
+								fontStyle={ titleFont[ 0 ].style }
+								onFontStyle={ ( value ) => saveTitleFont( { style: value } ) }
+								fontSubset={ titleFont[ 0 ].subset }
+								onFontSubset={ ( value ) => saveTitleFont( { subset: value } ) }
+								padding={ titleFont[ 0 ].padding }
+								onPadding={ ( value ) => saveTitleFont( { padding: value } ) }
+								paddingControl={ titleFont[ 0 ].paddingControl }
+								onPaddingControl={ ( value ) => saveTitleFont( { paddingControl: value } ) }
+								margin={ titleFont[ 0 ].margin }
+								onMargin={ ( value ) => saveTitleFont( { margin: value } ) }
+								marginControl={ titleFont[ 0 ].marginControl }
+								onMarginControl={ ( value ) => saveTitleFont( { marginControl: value } ) } />
+							</Fragment>
+						}
+					</PanelBody>
+
+					<PanelBody
+						title={ __( 'Number Settings' ) }
+						initialOpen={ true }>
+						<h2 className="kt-tab-wrap-title">{ __( 'Color Settings' ) }</h2>
+						<TabPanel className="kt-inspect-tabs kt-hover-tabs"
+							activeClass="active-tab"
+							tabs={ [
+								{
+									name: 'normal',
+									title: __( 'Normal' ),
+									className: 'kt-normal-tab',
+								},
+								{
+									name: 'hover',
+									title: __( 'Hover' ),
+									className: 'kt-hover-tab',
+								},
+							] }>
+							{
+								( tab ) => {
+									let tabout;
+									if ( tab.name ) {
+										if ( 'hover' === tab.name ) {
+											tabout = (
+												<AdvancedPopColorControl
+													label={ __( 'Hover Color' ) }
+													colorValue={ ( numberHoverColor ? numberHoverColor : '' ) }
+													colorDefault={ '' }
+													onColorChange={ value => setAttributes( { numberHoverColor: value } ) }
+												/>
+											);
+										} else {
+											tabout = (
+												<AdvancedPopColorControl
+													label={ __( 'Number Color' ) }
+													colorValue={ ( numberColor ? numberColor : '' ) }
+													colorDefault={ '' }
+													onColorChange={ value => setAttributes( { numberColor: value } ) }
+												/>
+											);
+										}
+									}
+									return <div className={ tab.className } key={ tab.className }>{ tabout }</div>;
+								}
+							}
+						</TabPanel>
+
+						<h2 className="kt-heading-size-title">{ __( 'Min Height' ) }</h2>
+						<TabPanel className="kt-size-tabs"
+							activeClass="active-tab"
+							tabs={ [
+								{
+									name: 'desk',
+									title: <Dashicon icon="desktop" />,
+									className: 'kt-desk-tab',
+								},
+								{
+									name: 'tablet',
+									title: <Dashicon icon="tablet" />,
+									className: 'kt-tablet-tab',
+								},
+								{
+									name: 'mobile',
+									title: <Dashicon icon="smartphone" />,
+									className: 'kt-mobile-tab',
+								},
+							] }>
+							{
+								( tab ) => {
+									let tabout;
+									if ( tab.name ) {
+										if ( 'mobile' === tab.name ) {
+											tabout = (
+												<KadenceRange
+													value={ ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 2 ] ) ? numberMinHeight[ 2 ] : '' ) }
+													onChange={ value => setAttributes( { numberMinHeight: [ ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 0 ] ) ? numberMinHeight[ 0 ] : '' ), ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 1 ] ) ? numberMinHeight[ 1 ] : '' ), value ] } ) }
+													step={ 1 }
+													min={ 0 }
+													max={ 600 }
+												/>
+											);
+										} else if ( 'tablet' === tab.name ) {
+											tabout = (
+												<KadenceRange
+													value={ ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 1 ] ) ? numberMinHeight[ 1 ] : '' ) }
+													onChange={ value => setAttributes( { numberMinHeight: [ ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 0 ] ) ? numberMinHeight[ 0 ] : '' ), value, ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 2 ] ) ? numberMinHeight[ 2 ] : '' ) ] } ) }
+													step={ 1 }
+													min={ 0 }
+													max={ 600 }
+												/>
+											);
+										} else {
+											tabout = (
+												<KadenceRange
+													value={ ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 0 ] ) ? numberMinHeight[ 0 ] : '' ) }
+													onChange={ value => setAttributes( { numberMinHeight: [ value, ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 1 ] ) ? numberMinHeight[ 1 ] : '' ), ( ( undefined !== numberMinHeight && undefined !== numberMinHeight[ 2 ] ) ? numberMinHeight[ 2 ] : '' ) ] } ) }
+													step={ 1 }
+													min={ 0 }
+													max={ 600 }
+												/>
+											);
+										}
+									}
+									return <div className={ tab.className } key={ tab.className }>{ tabout }</div>;
+								}
+							}
+						</TabPanel>
+						<TypographyControls
+							fontGroup={ 'heading' }
+							tagLevel={ numberFont[ 0 ].level }
+							onTagLevel={ ( value ) => saveNumberFont( { level: value } ) }
+							fontSize={ numberFont[ 0 ].size }
+							onFontSize={ ( value ) => saveNumberFont( { size: value } ) }
+							fontSizeType={ numberFont[ 0 ].sizeType }
+							onFontSizeType={ ( value ) => saveNumberFont( { sizeType: value } ) }
+							lineHeight={ numberFont[ 0 ].lineHeight }
+							onLineHeight={ ( value ) => saveNumberFont( { lineHeight: value } ) }
+							lineHeightType={ numberFont[ 0 ].lineType }
+							onLineHeightType={ ( value ) => saveNumberFont( { lineType: value } ) }
+							letterSpacing={ numberFont[ 0 ].letterSpacing }
+							onLetterSpacing={ ( value ) => saveNumberFont( { letterSpacing: value } ) }
+							fontFamily={ numberFont[ 0 ].family }
+							onFontFamily={ ( value ) => saveNumberFont( { family: value } ) }
+							onFontChange={ ( select ) => {
+								saveNumberFont( {
+									family: select.value,
+									google: select.google,
+								} );
+							} }
+							onFontArrayChange={ ( values ) => saveNumberFont( values ) }
+							googleFont={ numberFont[ 0 ].google }
+							onGoogleFont={ ( value ) => saveNumberFont( { google: value } ) }
+							loadGoogleFont={ numberFont[ 0 ].loadGoogle }
+							onLoadGoogleFont={ ( value ) => saveNumberFont( { loadGoogle: value } ) }
+							fontVariant={ numberFont[ 0 ].variant }
+							onFontVariant={ ( value ) => saveNumberFont( { variant: value } ) }
+							fontWeight={ numberFont[ 0 ].weight }
+							onFontWeight={ ( value ) => saveNumberFont( { weight: value } ) }
+							fontStyle={ numberFont[ 0 ].style }
+							onFontStyle={ ( value ) => saveNumberFont( { style: value } ) }
+							fontSubset={ numberFont[ 0 ].subset }
+							onFontSubset={ ( value ) => saveNumberFont( { subset: value } ) }
+							padding={ numberFont[ 0 ].padding }
+							onPadding={ ( value ) => saveNumberFont( { padding: value } ) }
+							paddingControl={ numberFont[ 0 ].paddingControl }
+							onPaddingControl={ ( value ) => saveNumberFont( { paddingControl: value } ) }
+							margin={ numberFont[ 0 ].margin }
+							onMargin={ ( value ) => saveNumberFont( { margin: value } ) }
+							marginControl={ numberFont[ 0 ].marginControl }
+							onMarginControl={ ( value ) => saveNumberFont( { marginControl: value } ) } />
 					</PanelBody>
 	            </InspectorControls>
 	        </Fragment>
