65cec328b4c2e6b956172c411bee081150b38aff
Josh Oakes <josh@joshoak.es>
2022-02-02 09:54:47 -0600

    Google Maps block

--- src/blocks/google-maps/edit.js
+++ src/blocks/google-maps/edit.js
@@ -12,7 +12,7 @@ import './editor.scss';
  */
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
-import { useBlockProps, BlockAlignmentControl } from '@wordpress/block-editor';
+import { useBlockProps } from '@wordpress/block-editor';
 import ResponsiveMeasurementControls from '../../components/measurement/responsive-measurement-control';
 const { rest_url } = kadence_blocks_params;
 import get from 'lodash/get';
@@ -20,20 +20,14 @@ import has from 'lodash/has';
 
 const {
 	InspectorControls,
-	BlockControls,
 } = wp.blockEditor;
 
 const { apiFetch } = wp;
 const {
 	PanelBody,
 	RangeControl,
-	ToggleControl,
 	TextControl,
-	Modal,
 	SelectControl,
-	FormFileUpload,
-	Button,
-	Notice,
 } = wp.components;
 
 
@@ -42,6 +36,8 @@ const {
  */
 import classnames from 'classnames';
 import ResponsiveRangeControl from '../../responsive-range-control'
+import ResponsiveRangeControls from '../../components/range/responsive-range-control'
+
 const ktlottieUniqueIDs = [];
 
 export function Edit( {
@@ -54,22 +50,17 @@ export function Edit( {
 
 	const {
 		uniqueID,
-		align,
-		width,
-		height,
+		heightDesktop,
+		heightTablet,
+		heightMobile,
+		widthDesktop,
+		widthTablet,
+		widthMobile,
 		location,
 		zoom,
 		mapType,
 		mapMode,
 		sizeSlug,
-		paddingTablet,
-		paddingDesktop,
-		paddingMobile,
-		paddingUnit,
-		marginTablet,
-		marginDesktop,
-		marginMobile,
-		marginUnit,
 	} = attributes;
 
 	const getPreviewSize = ( device, desktopSize, tabletSize, mobileSize ) => {
@@ -87,18 +78,8 @@ export function Edit( {
 		return desktopSize;
 	};
 
-	const previewMarginTop = getPreviewSize( previewDevice, ( undefined !== marginDesktop ? marginDesktop[0] : '' ), ( undefined !== marginTablet ? marginTablet[ 0 ] : '' ), ( undefined !== marginMobile ? marginMobile[ 0 ] : '' ) );
-	const previewMarginRight = getPreviewSize( previewDevice, ( undefined !== marginDesktop ? marginDesktop[1] : '' ), ( undefined !== marginTablet ? marginTablet[ 1 ] : '' ), ( undefined !== marginMobile ? marginMobile[ 1 ] : '' ) );
-	const previewMarginBottom = getPreviewSize( previewDevice, ( undefined !== marginDesktop ? marginDesktop[2] : '' ), ( undefined !== marginTablet ? marginTablet[ 2 ] : '' ), ( undefined !== marginMobile ? marginMobile[ 2 ] : '' ) );
-	const previewMarginLeft = getPreviewSize( previewDevice, ( undefined !== marginDesktop ? marginDesktop[3] : '' ), ( undefined !== marginTablet ? marginTablet[ 3 ] : '' ), ( undefined !== marginMobile ? marginMobile[ 3 ] : '' ) );
-
-	const previewPaddingTop = getPreviewSize( previewDevice, ( undefined !== paddingDesktop ? paddingDesktop[0] : '' ), ( undefined !== paddingTablet ? paddingTablet[ 0 ] : '' ), ( undefined !== paddingMobile ? paddingMobile[ 0 ] : '' ) );
-	const previewPaddingRight = getPreviewSize( previewDevice, ( undefined !== paddingDesktop ? paddingDesktop[1] : '' ), ( undefined !== paddingTablet ? paddingTablet[ 1 ] : '' ), ( undefined !== paddingMobile ? paddingMobile[ 1 ] : '' ) );
-	const previewPaddingBottom = getPreviewSize( previewDevice, ( undefined !== paddingDesktop ? paddingDesktop[2] : '' ), ( undefined !== paddingTablet ? paddingTablet[ 2 ] : '' ), ( undefined !== paddingMobile ? paddingMobile[ 2 ] : '' ) );
-	const previewPaddingLeft = getPreviewSize( previewDevice, ( undefined !== paddingDesktop ? paddingDesktop[3] : '' ), ( undefined !== paddingTablet ? paddingTablet[ 3 ] : '' ), ( undefined !== paddingMobile ? paddingMobile[ 3 ] : '' ) );
-
-	const [ marginControl, setMarginControl ] = useState( 'individual');
-	const [ paddingControl, setPaddingControl ] = useState( 'individual');
+	const previewHeight = getPreviewSize( previewDevice, ( undefined !== heightDesktop ? heightDesktop : '450' ), ( undefined !== heightTablet ? heightTablet : '450' ), ( undefined !== heightMobile ? heightMobile : '450' ) );
+	const previewWidth = getPreviewSize( previewDevice, ( undefined !== widthDesktop ? widthDesktop : '' ), ( undefined !== widthTablet ? widthTablet : '' ), ( undefined !== widthMobile ? widthMobile : '' ) );
 
 	const classes = classnames( className, {
 		[ `size-${ sizeSlug }` ]: sizeSlug,
@@ -130,6 +111,7 @@ export function Edit( {
 	let mapQueryParams = {
 		key: 'AIzaSyBGsB_DXmwf1WoHqBk0Jrt4VhyChI1mLjg',
 		zoom: zoom,
+		maptype: mapType,
 		q: location
 	};
 
@@ -139,12 +121,6 @@ export function Edit( {
 
 	return (
 		<figure { ...blockProps }>
-			<BlockControls group="block">
-				<BlockAlignmentControl
-					value={ align }
-					onChange={ ( value ) => setAttributes( { align: value } ) }
-				/>
-			</BlockControls>
 			<InspectorControls>
 				<PanelBody
 					title={ __('Map Location', 'kadence-blocks') }
@@ -169,7 +145,7 @@ export function Edit( {
 
 					<TextControl
 						label={ ( mapMode === 'search' ? __('Search', 'kadence-blocks') : __('Pinpoint', 'kadence-blocks') ) }
-						help={ ( mapMode === 'search' ? __('Example: Starbucks, Midtown NYC', 'kadence-blocks') : __('Example: 42 Wallaby Way, Sydney', 'kadence-blocks') ) }
+						help={ ( mapMode === 'search' ? __('Example: BBQ in Austin, TX', 'kadence-blocks') : __('Example: 42 Wallaby Way, Sydney', 'kadence-blocks') ) }
 						value={ location }
 						onChange={ (value) => setAttributes( { location: value} ) }
 					/>
@@ -196,89 +172,60 @@ export function Edit( {
 							}
 						] } />
 
-					<ResponsiveRangeControl
-						label={ __( 'Height', 'kadence-blocks') }
-						value={ parseInt( height ) }
-						onChange={ (value) => setAttributes( { height: value } ) }
-						units={ [ 'px', 'em', 'rem', '%' ] }
-						min={ 100 }
-						max={ 2000 }
-					/>
-
 				</PanelBody>
 
 				<PanelBody
 					title={ __( 'Size Controls', 'kadence-blocks' ) }
 					initialOpen={ false }
 				>
-					<ResponsiveMeasurementControls
-						label={ __( 'Padding', 'kadence-blocks' ) }
-						value={ [ previewPaddingTop, previewPaddingRight, previewPaddingBottom, previewPaddingLeft ] }
-						control={ paddingControl }
-						tabletValue={ paddingTablet }
-						mobileValue={ paddingMobile }
-						onChange={ ( value ) => setAttributes( { paddingDesktop: value } ) }
-						onChangeTablet={ ( value ) => setAttributes( { paddingTablet: value } ) }
-						onChangeMobile={ ( value ) => setAttributes( { paddingMobile: value } ) }
-						onChangeControl={ ( value ) => setPaddingControl( value ) }
-						min={ 0 }
-						max={ ( paddingUnit === 'em' || paddingUnit === 'rem' ? 24 : 200 ) }
-						step={ ( paddingUnit === 'em' || paddingUnit === 'rem' ? 0.1 : 1 ) }
-						unit={ paddingUnit }
-						units={ [ 'px', 'em', 'rem', '%' ] }
-						onUnit={ ( value ) => setAttributes( { paddingUnit: value } ) }
-					/>
-					<ResponsiveMeasurementControls
-						label={ __( 'Margin', 'kadence-blocks' ) }
-						value={ [ previewMarginTop, previewMarginRight, previewMarginBottom, previewMarginLeft ] }
-						control={ marginControl }
-						tabletValue={ marginTablet }
-						mobileValue={ marginMobile }
-						onChange={ ( value ) => {
-							setAttributes( { marginDesktop: [ value[ 0 ], value[ 1 ], value[ 2 ], value[ 3 ] ] } );
-						} }
-						onChangeTablet={ ( value ) => setAttributes( { marginTablet: value } ) }
-						onChangeMobile={ ( value ) => setAttributes( { marginMobile: value } ) }
-						onChangeControl={ ( value ) => setMarginControl( value ) }
-						min={ ( marginUnit === 'em' || marginUnit === 'rem' ? -12 : -200 ) }
-						max={ ( marginUnit === 'em' || marginUnit === 'rem' ? 24 : 200 ) }
-						step={ ( marginUnit === 'em' || marginUnit === 'rem' ? 0.1 : 1 ) }
-						unit={ marginUnit }
-						units={ [ 'px', 'em', 'rem', '%', 'vh' ] }
-						onUnit={ ( value ) => setAttributes( { marginUnit: value } ) }
+					<ResponsiveRangeControls
+						label={ __( 'Height', 'kadence-blocks' ) }
+						value={ heightDesktop }
+						onChange={ value => setAttributes( { heightDesktop: value } ) }
+						tabletValue={ ( heightTablet ? heightTablet : '' ) }
+						onChangeTablet={ ( value ) => setAttributes( { heightTablet: value } ) }
+						mobileValue={ ( heightMobile ? heightMobile : '' ) }
+						onChangeMobile={ ( value ) => setAttributes( { heightMobile: value } ) }
+						min={ 100 }
+						max={ 1250 }
+						step={ 1 }
+						unit={ 'px' }
+						units={ [ 'px' ] }
+						showUnit={ true }
 					/>
 
-					<RangeControl
-						label={ __( 'Max Width' ) }
-						value={ width }
-						onChange={ ( value ) => setAttributes( { width: value } ) }
-						allowReset={ true }
+					<ResponsiveRangeControls
+						label={ __( 'Max Width', 'kadence-blocks' ) }
+						value={ widthDesktop }
+						onChange={ value => setAttributes( { widthDesktop: value } ) }
+						tabletValue={ ( widthTablet ? widthTablet : '' ) }
+						onChangeTablet={ ( value ) => setAttributes( { widthTablet: value } ) }
+						mobileValue={ ( widthMobile ? heightMobile : '' ) }
+						onChangeMobile={ ( value ) => setAttributes( { widthMobile: value } ) }
+						min={ 100 }
+						max={ 1250 }
 						step={ 1 }
-						min={ 25 }
-						max={ 1000 }
+						unit={ 'px' }
+						units={ [ 'px' ] }
+						showUnit={ true }
+						reset={ () => setAttributes( { widthDesktop: '', widthTablet: '', widthMobile: '' } ) }
 					/>
+
+
 				</PanelBody>
 			</InspectorControls>
-			<div style={
-				{
-					marginTop: ( '' !== previewMarginTop ? previewMarginTop + marginUnit : undefined ),
-					marginRight: ( '' !== previewMarginRight ? previewMarginRight + marginUnit : undefined ),
-					marginBottom: ( '' !== previewMarginBottom ? previewMarginBottom + marginUnit : undefined ),
-					marginLeft: ( '' !== previewMarginLeft ? previewMarginLeft + marginUnit : undefined ),
 
-					paddingTop: ( '' !== previewPaddingTop ? previewPaddingTop + paddingUnit : undefined ),
-					paddingRight: ( '' !== previewPaddingRight ? previewPaddingRight + paddingUnit : undefined ),
-					paddingBottom: ( '' !== previewPaddingBottom ? previewPaddingBottom + paddingUnit : undefined ),
-					paddingLeft: ( '' !== previewPaddingLeft ? previewPaddingLeft + paddingUnit : undefined ),
-				}
-			}>
-				<iframe width={ '100%' } height={ height + 'px' } style={ { border: '0' } } loading={ 'lazy' }
+			<div className={ 'kb-map-container' } style={ {
+				height: previewHeight + 'px',
+				maxWidth: ( previewWidth === '' ? '100%' : previewWidth + 'px' ),
+			} }>
+				<div className={ 'kb-map-container-infobar' }>
+				</div>
+				<iframe width={ '100%' } height={ '100%' } loading={ 'lazy' }
 								src={ 'https://www.google.com/maps/embed/v1/' + mapMode + '?' + qs }></iframe>
 			</div>
 		</figure>
 	);
 }
-
-// AIzaSyBGsB_DXmwf1WoHqBk0Jrt4VhyChI1mLjg
 
 export default ( Edit );
--- src/blocks/google-maps/save.js
+++ src/blocks/google-maps/save.js
@@ -18,17 +18,28 @@ import { InnerBlocks } from '@wordpress/block-editor';
 const apiKey = 'AIzaSyBGsB_DXmwf1WoHqBk0Jrt4VhyChI1mLjg';
 
 function Save( { attributes } ) {
-	const { uniqueID } = attributes;
+	const { uniqueID, mapMode, zoom, mapType, location } = attributes;
 	const classes = classnames( {
 		'kb-google-maps-container': true,
 		[ `kb-google-maps-container${ uniqueID }` ] : true,
 		[ `align${ attributes.align }` ]: attributes.align,
 	} );
 
+	let mapQueryParams = {
+		key: 'AIzaSyBGsB_DXmwf1WoHqBk0Jrt4VhyChI1mLjg',
+		zoom: zoom,
+		maptype: mapType,
+		q: location
+	};
+
+	const qs = Object.keys(mapQueryParams)
+		.map(key => `${encodeURIComponent(key)}=${encodeURIComponent(mapQueryParams[key])}`)
+		.join('&');
+
 	return (
 		<div className={ classes }>
-			<iframe width={ '600px' } height={ '450px' } style={ { border: '0' } } loading={ 'lazy' }
-							src={ 'https://maps.google.com/maps?q=336%20Madison%20Place%2C%20Dr.%20Moore%2C%20OK&t=m&z=10&output=embed&iwloc=near' }></iframe>
+			<iframe width={ '100%' } height={ '100%' } style={ { border: '0' } } loading={ 'lazy' }
+							src={ 'https://www.google.com/maps/embed/v1/' + mapMode + '?' + qs }></iframe>
 		</div>
 	);
 }
