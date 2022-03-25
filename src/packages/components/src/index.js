8773f29d3769ca791a0b848ac6adf9742fa55d4e
Josh Oakes <josh@joshoak.es>
2022-03-24 19:12:27 -0500

    Move PopColorControl Component

---
+++ src/packages/components/src/index.js
@@ -0,0 +1,5 @@
+export { default as PopColorControl } from './pop-color-control';
+export { default as SinglePopColorControl } from './single-pop-color-control';
+export { default as ColorIcons } from './color-icons';
+export { default as ColorPicker } from './color-picker';
+export { default as ColorFields } from './color-fields';
