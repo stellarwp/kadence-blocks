d323af54f44d3ec6feb892de50e5c42cf789c3f5
Josh Oakes <josh@joshoak.es>
2022-04-06 14:03:47 -0500

    Create icons

---
+++ src/packages/icons/src/vtabs/index.js
@@ -0,0 +1,5 @@
+export default <svg width="20px" height="20px" viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg' fillRule='evenodd' clipRule='evenodd' strokeLinejoin='round' strokeMiterlimit='1.414'>
+	<path d='M17.227,17.294l0,-14.588l-14.454,0l0.002,3.319l3.473,-0.003l0.006,11.272c4.339,0 6.634,0 10.973,0Zm-0.759,-13.738l0,12.888l-9.455,0l0.001,-11.251l-3.542,-0.008l0.002,-1.629c3.833,0 9.161,0 12.994,0Z' fill='#555d66' fillRule='nonzero' />
+	<path d='M2.82,9.846l0,-3.033l2.669,0l0,3.033l-2.669,0Z' fill='#555d66' />
+	<path d='M2.823,13.676l0,-3.011l2.656,0l0,3.011l-2.656,0Z' fill='#555d66' />
+</svg>;
