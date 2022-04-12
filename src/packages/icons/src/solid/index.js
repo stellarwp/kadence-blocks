b8aef8dd130ab2137805de07bcd54d2001ac567e
Josh Oakes <josh@joshoak.es>
2022-04-11 21:15:27 -0500

    Create remaining icons

---
+++ src/packages/icons/src/solid/index.js
@@ -0,0 +1,10 @@
+export default <svg width="24" height="24"
+					xmlns="http://www.w3.org/2000/svg"
+					fillRule="evenodd"
+					strokeLinejoin="round"
+					strokeMiterlimit="2"
+					clipRule="evenodd"
+					viewBox="0 0 20 20"
+>
+	<path d="M18.988 11.478V8.522H1.012v2.956h17.976z"></path>
+</svg>;
