89bee364a9956c28fc666cdb6369e19777bba187
Josh Oakes <josh@joshoak.es>
2022-04-12 14:47:09 -0500

    Place block specific icons in their own folder

--- src/packages/icons/src/h1/index.js
+++
@@ -1,7 +0,0 @@
-export default <svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd"
-					clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1.414">
-	<path d="M9.229,3.5l0,5.2l-5,0l0,-5.2l-2.5,0l0,13l2.5,0l0,-5.2l5,0l0,5.2l2.5,0l0,-13l-2.5,0Z"
-		  fillRule="nonzero" />
-	<path d="M12.993,9.914c0.232,-0.096 0.474,-0.206 0.726,-0.33c0.252,-0.124 0.498,-0.26 0.738,-0.408c0.24,-0.148 0.47,-0.304 0.69,-0.468c0.22,-0.164 0.418,-0.338 0.594,-0.522l1.248,0l0,8.316l-1.788,0l0,-5.988c-0.24,0.16 -0.508,0.306 -0.804,0.438c-0.296,0.132 -0.584,0.246 -0.864,0.342l-0.54,-1.38Z"
-		  fillRule="nonzero" />
-</svg>;
