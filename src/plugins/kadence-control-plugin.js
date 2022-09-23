e50137be160e13bc551235b0908739697eb65ce8
Josh Oakes <josh@joshoak.es>
2022-09-23 17:21:03 -0500

    Create reusable component for block settings visibility
    Begin implementing it on InfoBox & Countdown blocks

--- src/plugins/kadence-control-plugin.js
+++ src/plugins/kadence-control-plugin.js
@@ -1,24 +1,25 @@
+import {map} from 'lodash';
 
-import { map } from 'lodash';
-
-import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
-import { __ } from '@wordpress/i18n';
+import {PluginSidebar, PluginSidebarMoreMenuItem} from '@wordpress/edit-post';
+import {__} from '@wordpress/i18n';
 import {
-	withSelect,
+    withSelect,
 } from '@wordpress/data';
 import {
-	Component,
-	Fragment,
+    Component,
+    Fragment,
+    useState
 } from '@wordpress/element';
 import {
-	PanelBody,
+    PanelBody,
 } from '@wordpress/components';
-import { compose } from '@wordpress/compose';
-import { applyFilters } from '@wordpress/hooks';
+import {compose} from '@wordpress/compose';
+import {applyFilters} from '@wordpress/hooks';
 /**
  * Import Icons
  */
-import { kadenceNewIcon } from '@kadence/icons';
+import {kadenceNewIcon} from '@kadence/icons';
+
 
 /**
  * Import Settings
@@ -31,7 +32,6 @@ import KadenceTabsSettings from './block-settings/tabs-settings';
 import KadenceAccordion from './block-defaults/accordion-defaults';
 import KadenceAccordionSettings from './block-settings/accordion-settings';
 import KadenceInfoBox from './block-defaults/info-box-defaults';
-import KadenceInfoBoxSettings from './block-settings/info-box-settings';
 import KadenceAdvancedBtn from './block-defaults/advanced-button-defaults';
 import KadenceAdvancedSettings from './block-settings/advanced-button-settings';
 import KadenceIconList from './block-defaults/icon-list-defaults';
@@ -49,176 +49,176 @@ import KadenceColumnSettings from './block-settings/column-settings';
 import KadenceColors from './block-defaults/color-palette-defaults';
 import KadenceDesignLibrarySettings from './block-settings/design-library-settings';
 
+import KadenceCountdownSettings from "./block-settings/countdown-settings";
+import KadenceCountdown from "./block-defaults/countdown-defaults";
+
 /*
  * Components
  */
 import KadenceFontFamily from './block-defaults/typography-defaults';
+import KadenceVisibilitySettings from "./block-settings/block-visability-settings";
+
 /**
  * Build the row edit
  */
-class KadenceConfig extends Component {
-	constructor() {
-		super( ...arguments );
-		this.state = {
-			user: ( kadence_blocks_params.userrole ? kadence_blocks_params.userrole : 'admin' ),
-			controls: applyFilters( 'kadence.block_controls_sidebar', [] ),
-			blocks: applyFilters( 'kadence.block_blocks_sidebar', [] ),
-			extraPanels: applyFilters( 'kadence.block_panels_sidebar', [] ),
-			deviceType: 'Desktop',
-		};
-	}
-	render() {
-		const changeBodyClass = ( key ) => {
-			if ( key ) {
-				document.body.classList.remove( 'kadence-preview-width-tablet' );
-				document.body.classList.remove( 'kadence-preview-width-mobile' );
-				document.body.classList.remove( 'kadence-preview-width-desktop' );
-				document.body.classList.add( 'kadence-preview-width-' + key.toLowerCase() );
-			}
-		};
-		if ( this.state.deviceType !== this.props.getPreviewDevice ) {
-			this.setState( { deviceType:this.props.getPreviewDevice } );
-			changeBodyClass( this.props.getPreviewDevice );
-		}
-		return (
-			<Fragment>
-				<PluginSidebarMoreMenuItem
-					target="kadence-controls"
-					icon={ kadenceNewIcon }
-				>
-					{ __( 'Kadence Blocks Controls', 'kadence-blocks' ) }
-				</PluginSidebarMoreMenuItem>
-				<PluginSidebar
-					isPinnable={ true }
-					name="kadence-controls"
-					title={ __( 'Kadence Blocks Controls', 'kadence-blocks'  ) }
-				>
-					<PanelBody
-						title={ __( 'Color Palette', 'kadence-blocks'  ) }
-						initialOpen={ true }
-					>
-						<KadenceColors />
-					</PanelBody>
-					<PanelBody
-						title={ __( 'Block Defaults', 'kadence-blocks'  ) }
-					>
-						<div className="kt-blocks-control-wrap">
-							{/*Spacer*/}
-							<div className="kt-blocks-control-row">
-								<KadenceSpacer />
-								{ 'admin' === this.state.user && (
-									<KadenceSpacerSettings />
-								) }
-							</div>
-							{/*Tabs*/}
-							<div className="kt-blocks-control-row">
-								<KadenceTabs />
-								{ 'admin' === this.state.user && (
-									<KadenceTabsSettings />
-								) }
-							</div>
-							{/*Accordion*/}
-							<div className="kt-blocks-control-row">
-								<KadenceAccordion />
-								{ 'admin' === this.state.user && (
-									<KadenceAccordionSettings />
-								) }
-							</div>
-							{/*Info Box*/}
-							<div className="kt-blocks-control-row">
-								<KadenceInfoBox />
-								{ 'admin' === this.state.user && (
-									<KadenceInfoBoxSettings />
-								) }
-							</div>
-							{/*Advanced Button*/}
-							<div className="kt-blocks-control-row">
-								<KadenceAdvancedBtn />
-								{ 'admin' === this.state.user && (
-									<KadenceAdvancedSettings />
-								) }
-							</div>
-							{/*Icon List*/}
-							<div className="kt-blocks-control-row">
-								<KadenceIconList />
-								{ 'admin' === this.state.user && (
-									<KadenceIconListSettings />
-								) }
-							</div>
-							{/*Testimonials*/}
-							<div className="kt-blocks-control-row">
-								<KadenceTestimonials />
-								{ 'admin' === this.state.user && (
-									<KadenceTestimonialsSettings />
-								) }
-							</div>
-							{/*Advanced Heading*/}
-							<div className="kt-blocks-control-row">
-								<KadenceHeadings />
-								{ 'admin' === this.state.user && (
-									<KadenceHeadingsSettings />
-								) }
-							</div>
-							{/*Row Layout*/}
-							<div className="kt-blocks-control-row">
-								<KadenceRowLayout />
-								{ 'admin' === this.state.user && (
-									<KadenceRowLayoutSettings />
-								) }
-							</div>
-							{/*Section*/}
-							<div className="kt-blocks-control-row">
-								<KadenceColumn />
-								{ 'admin' === this.state.user && (
-									<KadenceColumnSettings />
-								) }
-							</div>
-							{/*Advanced Gallery*/}
-							<div className="kt-blocks-control-row">
-								<KadenceGallery />
-								{ 'admin' === this.state.user && (
-									<KadenceGallerySettings />
-								) }
-							</div>
-							{ map( this.state.blocks, ( { Control } ) => (
-								<Control />
-							) ) }
-							<h3>{ __( 'Components', 'kadence-blocks'  ) }</h3>
-							<KadenceFontFamily />
-							<KadenceDesignLibrarySettings />
-							{ map( this.state.controls, ( { Control } ) => (
-								<Control />
-							) ) }
-						</div>
-					</PanelBody>
-					{ ( ( undefined === kadence_blocks_params ) || ( undefined !== kadence_blocks_params && undefined === kadence_blocks_params.editor_width ) || ( undefined !== kadence_blocks_params && undefined !== kadence_blocks_params.editor_width && kadence_blocks_params.editor_width ) ) && (
-						<PanelBody
-							title={ __( 'Editor Width', 'kadence-blocks'  ) }
-							initialOpen={ false }
-						>
-							<KadenceEditorWidth />
-						</PanelBody>
-					) }
-					{/* <PanelBody
+function KadenceConfig({getPreviewDevice}) {
+
+    const [user, setUser] = useState((kadence_blocks_params.userrole ? kadence_blocks_params.userrole : 'admin'));
+    const [controls, setControls] = useState(applyFilters('kadence.block_controls_sidebar', []));
+    const [blocks, setBlocks] = useState(applyFilters('kadence.block_blocks_sidebar', []));
+    const [extraPanels, setExtraPanels] = useState(applyFilters('kadence.block_panels_sidebar', []));
+
+    return (
+        <Fragment>
+            <PluginSidebarMoreMenuItem
+                target="kadence-controls"
+                icon={kadenceNewIcon}
+            >
+                {__('Kadence Blocks Controls', 'kadence-blocks')}
+            </PluginSidebarMoreMenuItem>
+            <PluginSidebar
+                isPinnable={true}
+                name="kadence-controls"
+                title={__('Kadence Blocks Controls', 'kadence-blocks')}
+            >
+                <PanelBody
+                    title={__('Color Palette', 'kadence-blocks')}
+                    initialOpen={true}
+                >
+                    <KadenceColors/>
+                </PanelBody>
+                <PanelBody
+                    title={__('Block Defaults', 'kadence-blocks')}
+                >
+                    <div className="kt-blocks-control-wrap">
+                        {/*Accordion*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceAccordion/>
+                            {'admin' === user && (
+                                <KadenceAccordionSettings/>
+                            )}
+                        </div>
+                        {/*Advanced Button*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceAdvancedBtn/>
+                            {'admin' === user && (
+                                <KadenceAdvancedSettings/>
+                            )}
+                        </div>
+                        {/*Advanced Gallery*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceGallery/>
+                            {'admin' === user && (
+                                <KadenceGallerySettings/>
+                            )}
+                        </div>
+                        {/*Advanced Heading*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceHeadings/>
+                            {'admin' === user && (
+                                <KadenceHeadingsSettings/>
+                            )}
+                        </div>
+                        {/*Countdown*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceCountdown/>
+                            {'admin' === user && (
+                                <KadenceVisabilitySettings blockName={'Countdown'} blockSlug={'countdown'} options={ [
+                                    { key: 'textStyle', label: __( 'Enable Text Style Settings', 'kadence-blocks' ) },
+                                    { key: 'joinedIcons', label: __( 'Enable All List Icon Control', 'kadence-blocks' ) },
+                                    { key: 'individualIcons', label: __( 'Enable individual List Item Control', 'kadence-blocks' ) },
+                                ] } />
+                            )}
+                        </div>
+                        {/*Info Box*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceInfoBox/>
+                            {'admin' === user && (
+                                <KadenceVisibilitySettings blockName={'Info Box'} blockSlug={'infobox'} options={ [
+                                    { key: 'containerSettings', label: __( 'Enable Container Settings', 'kadence-blocks' ) },
+                                    { key: 'mediaSettings', label: __( 'Enable Media Settings', 'kadence-blocks' ) },
+                                    { key: 'titleSettings', label: __( 'Enable Title Settings', 'kadence-blocks' ) },
+                                    { key: 'textSettings', label: __( 'Enable Text Settings', 'kadence-blocks' ) },
+                                    { key: 'learnMoreSettings', label: __( 'Enable Learn More Settings', 'kadence-blocks' ) },
+                                    { key: 'shadowSettings', label: __( 'Enable Shadow Settings', 'kadence-blocks' ) }
+                                ] } />
+                            )}
+                        </div>
+                        {/*Icon List*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceIconList/>
+                            {'admin' === user && (
+                                <KadenceIconListSettings/>
+                            )}
+                        </div>
+                        {/*Row Layout*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceRowLayout/>
+                            {'admin' === user && (
+                                <KadenceRowLayoutSettings/>
+                            )}
+                        </div>
+                        {/*Section*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceColumn/>
+                            {'admin' === user && (
+                                <KadenceColumnSettings/>
+                            )}
+                        </div>
+                        {/*Spacer*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceSpacer/>
+                            {'admin' === user && (
+                                <KadenceSpacerSettings/>
+                            )}
+                        </div>
+                        {/*Tabs*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceTabs/>
+                            {'admin' === user && (
+                                <KadenceTabsSettings/>
+                            )}
+                        </div>
+                        {/*Testimonials*/}
+                        <div className="kt-blocks-control-row">
+                            <KadenceTestimonials/>
+                            {'admin' === user && (
+                                <KadenceTestimonialsSettings/>
+                            )}
+                        </div>
+                        {map(blocks, ({Control}) => (
+                            <Control/>
+                        ))}
+                        <h3>{__('Components', 'kadence-blocks')}</h3>
+                        <KadenceFontFamily/>
+                        <KadenceDesignLibrarySettings/>
+                        {map(controls, ({Control}) => (
+                            <Control/>
+                        ))}
+                    </div>
+                </PanelBody>
+                {((undefined === kadence_blocks_params) || (undefined !== kadence_blocks_params && undefined === kadence_blocks_params.editor_width) || (undefined !== kadence_blocks_params && undefined !== kadence_blocks_params.editor_width && kadence_blocks_params.editor_width)) && (
+                    <PanelBody
+                        title={__('Editor Width', 'kadence-blocks')}
+                        initialOpen={false}
+                    >
+                        <KadenceEditorWidth/>
+                    </PanelBody>
+                )}
+                {/* <PanelBody
 						title={ __( 'Global Styles' ) }
 						initialOpen={ false }
 					>
-						{ 'admin' === this.state.user && (
+						{ 'admin' === user && (
 							<KadenceGlobalTypography />
 						) }
 					</PanelBody> */}
-					{ map( this.state.extraPanels, ( { Panel } ) => (
-						<Panel />
-					) ) }
-				</PluginSidebar>
-			</Fragment>
-		);
-	}
+                {map(extraPanels, ({Panel}) => (
+                    <Panel/>
+                ))}
+            </PluginSidebar>
+        </Fragment>
+    );
 }
-export default compose( [
-	withSelect( ( select, ownProps ) => {
-		return {
-			getPreviewDevice: select( 'kadenceblocks/data' ).getPreviewDeviceType(),
-		};
-	} ),
-] )( KadenceConfig );
+
+export default KadenceConfig;
