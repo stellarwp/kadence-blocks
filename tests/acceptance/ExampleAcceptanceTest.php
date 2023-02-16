4b7e3f07d3f2e06c738ae91b878a09e9ada72043
Josh Oakes <josh@joshoak.es>
2023-02-15 19:08:53 -0600

    Run on demand

---
+++ tests/acceptance/ExampleAcceptanceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class ExampleAcceptanceTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \AcceptanceTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
---
+++ tests/functional/ExampleFunctionalTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class ExampleFunctionalTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \FunctionalTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
