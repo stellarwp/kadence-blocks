bf5e7ee5073df794cffa982f331b6efa6e5fa177
Josh Oakes <joakes@ithemes.com>
2023-04-24 18:18:01 -0500

    Use Modal instead of alert for rename

--- src/plugins/block-rename/block-rename.js
+++ src/plugins/block-rename/block-rename.js
@@ -1,47 +1,92 @@
+import { get, startsWith } from 'lodash';
 import { registerPlugin } from '@wordpress/plugins';
+import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { BlockSettingsMenuControls } from '@wordpress/block-editor';
-import { MenuItem } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
-import { get, startsWith } from 'lodash';
+import {
+	MenuItem,
+	Modal,
+	TextControl,
+	Button,
+} from '@wordpress/components';
 
 const RenameBlockMenuItem = () => {
 	const { getBlock } = useSelect( 'core/block-editor' );
 	const { updateBlockAttributes } = useDispatch( 'core/block-editor' );
 
-	const handleRenameClick = ( block ) => {
-		const newBlockName = prompt( __( 'Select a new name for this block', 'kadence-blocks' ), block.attributes.metadata.name || '' );
-
-		if ( newBlockName !== null ) {
-			updateBlockAttributes( block.clientId, { metadata: { ...block.attributes.metadata, name: newBlockName } } );
-		}
+	const [ isOpen, setOpen ] = useState( false );
+	const [ block, setBlock ] = useState( null );
+	const [ tmpName, setTmpName ] = useState( '' );
+	const closeModal = () => setOpen( false );
+	const openModal = ( blockData ) => {
+		setBlock( blockData );
+		setTmpName( get( blockData, [ 'attributes', 'metadata', 'name' ], '' ) );
+		setOpen( true );
 	};
 
 	return (
-		<BlockSettingsMenuControls>
-			{( props ) => {
-				const { selectedClientIds, selectedBlocks, onClose } = props;
+		<>
+			<BlockSettingsMenuControls>
+				{( props ) => {
+					const { selectedClientIds, selectedBlocks, onClose } = props;
 
-				if ( selectedClientIds.length !== 1 || !startsWith( get( selectedBlocks, [ 0 ] ), 'kadence' ) ) {
-					return null;
-				}
+					if ( selectedClientIds.length !== 1 || !startsWith( get( selectedBlocks, [ 0 ] ), 'kadence/' ) ) {
+						return null;
+					}
 
-				return (
-					<MenuItem
-						onClick={() => {
-							handleRenameClick( getBlock( selectedClientIds[ 0 ] ) );
-							onClose();
+					return (
+						<MenuItem
+							onClick={() => {
+								openModal( getBlock( selectedClientIds[ 0 ] ) );
+								onClose();
+							}}
+							icon={'smiley'}
+							iconPosition={null}
+							label={'Rename'}
+							role={null}
+						>
+							Rename
+						</MenuItem>
+					);
+				}}
+			</BlockSettingsMenuControls>
+
+			{isOpen && (
+				<Modal title={__( 'Rename Block', 'kadence-blocks' )} onRequestClose={closeModal} focusOnMount={false}>
+					<TextControl
+						label={__( 'Block Name', 'kadence-blocks' )}
+						value={tmpName}
+						autoFocus={true}
+						onFocus={( event ) => event.target.select()}
+						onChange={( value ) => {
+							setTmpName( value );
 						}}
-						icon={'smiley'}
-						iconPosition={null}
-						label={'Rename'}
-						role={null}
-					>
-						Rename
-					</MenuItem>
-				);
-			}}
-		</BlockSettingsMenuControls>
+					/>
+
+					<div style={ { display: 'flex', flexDirection: 'row-reverse' } }>
+						<Button
+							isPrimary
+							style={ { marginLeft: '15px' } }
+							onClick={() => {
+								updateBlockAttributes( block.clientId, { metadata: { ...block.attributes.metadata, name: tmpName } } );
+								closeModal();
+							}}>
+							{__( 'Save', 'kadence-blocks' )}
+						</Button>
+
+						<Button
+							isDestructive
+							onClick={() => {
+								closeModal();
+								setTmpName( '' );
+							}}>
+							{__( 'Cancel', 'kadence-blocks' )}
+						</Button>
+					</div>
+				</Modal>
+			)}
+		</>
 	);
 
 };
