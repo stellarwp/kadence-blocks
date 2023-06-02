dbcbb0344406a1bd80df507b63c0c5c1adb54486
Ben Ritner <ben@kadencewp.com>
2023-06-02 10:40:58 -0600

    safari bug with buttons

--- includes/blocks/class-kadence-blocks-singlebtn-block.php
+++ includes/blocks/class-kadence-blocks-singlebtn-block.php
@@ -150,12 +150,17 @@ class Kadence_Blocks_Singlebtn_Block extends Kadence_Blocks_Abstract_Block {
 			}
 		}
 		// Hover before.
-		$css->set_selector( '.kb-btn' . $unique_id . '.kb-button:hover::before' );
 		if ( 'gradient' === $bg_type && 'normal' === $bg_hover_type && ! empty( $attributes['backgroundHover'] ) ) {
+			$css->set_selector( '.kb-btn' . $unique_id . '.kb-button:hover::before' );
 			$css->add_property( 'background', $css->render_color( $attributes['backgroundHover'] ) );
+			$css->set_selector( '.kb-btn' . $unique_id . '.kb-button::before' );
+			$css->add_property( 'transition', 'opacity .3s ease-in-out' );
 		}
 		if ( 'gradient' === $bg_hover_type && ! empty( $attributes['gradientHover'] ) ) {
+			$css->set_selector( '.kb-btn' . $unique_id . '.kb-button:hover::before' );
 			$css->add_property( 'background', $attributes['gradientHover'] );
+			$css->set_selector( '.kb-btn' . $unique_id . '.kb-button::before' );
+			$css->add_property( 'transition', 'opacity .3s ease-in-out' );
 		}
 		// Only Icon.
 		if ( isset( $attributes['onlyIcon'][0] ) && '' !== $attributes['onlyIcon'][0] && true == $attributes['onlyIcon'][0] ) {
