2c730f429269b1e42765f9a35c9d6030d5e3a8ae
Josh Oakes <joakes@ithemes.com>
2023-08-11 19:16:01 -0500

    test

--- bin/setup-docker.sh
+++ bin/setup-docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+ls
 curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ./bin/test-up.sh
