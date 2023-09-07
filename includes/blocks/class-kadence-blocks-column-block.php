7cc4c56c0729ce30cd456afcd1046d9534e49808
Mark Woodard <mark.c.woodard@gmail.com>
2023-09-07 17:12:21 -0500

    fixing an issue with mobile row gaps on mobil vertical layouts for the column block

--- includes/blocks/class-kadence-blocks-column-block.php
+++ includes/blocks/class-kadence-blocks-column-block.php
@@ -562,6 +562,14 @@ class Kadence_Blocks_Column_Block extends Kadence_Blocks_Abstract_Block {
 				$row_gap      = isset( $attributes['rowGap'][1] ) && is_numeric( $attributes['rowGap'][1] ) ? $attributes['rowGap'][1] : 0;
 				$row_gap_unit = ! empty( $attributes['rowGapUnit'] ) ? $attributes['rowGapUnit'] : 'px';
 				$css->add_property( 'row-gap', $row_gap . $row_gap_unit );
+
+				// The previous condition might have set us to block so we need to use margin instead of gap.
+				if ( $desktop_direction === 'horizontal' ) {
+					$css->set_selector( '.kadence-column' . $unique_id . ' > .kt-inside-inner-col > *:not(:last-child)' );
+					$css->add_property( 'margin-bottom', $row_gap . $row_gap_unit );
+				} else {
+					$css->add_property( 'row-gap', $row_gap . $row_gap_unit );
+				}
 			}
 		} elseif ( 'horizontal' === $tablet_direction ) {
 			if( !empty( $attributes['flexBasis'][1] ) ) {
@@ -671,14 +679,21 @@ class Kadence_Blocks_Column_Block extends Kadence_Blocks_Abstract_Block {
 				$css->add_property( 'display', 'block' );
 				$css->add_property( 'margin-left', '0px' );
 				$css->set_selector( '.kadence-column' . $unique_id . ' > .kt-inside-inner-col > *' );
-				$css->add_property( 'margin-left', '0px' );;
+				$css->add_property( 'margin-left', '0px' );
 			}
 
 			if ( ! empty( $attributes['rowGap'][2] ) ) {
 				$css->set_selector( '.kadence-column' . $unique_id . ' > .kt-inside-inner-col' );
 				$row_gap      = isset( $attributes['rowGap'][2] ) && is_numeric( $attributes['rowGap'][2] ) ? $attributes['rowGap'][2] : 0;
 				$row_gap_unit = ! empty( $attributes['rowGapUnit'] ) ? $attributes['rowGapUnit'] : 'px';
-				$css->add_property( 'row-gap', $row_gap . $row_gap_unit );
+
+				// The previous condition might have set us to block so we need to use margin instead of gap.
+				if ( $desktop_direction === 'horizontal' || $tablet_direction === 'horizontal' ) {
+					$css->set_selector( '.kadence-column' . $unique_id . ' > .kt-inside-inner-col > *:not(:last-child)' );
+					$css->add_property( 'margin-bottom', $row_gap . $row_gap_unit );
+				} else {
+					$css->add_property( 'row-gap', $row_gap . $row_gap_unit );
+				}
 			}
 		} elseif ( 'horizontal' === $mobile_direction ) {
 
