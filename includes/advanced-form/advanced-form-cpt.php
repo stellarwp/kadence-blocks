daea4cdcb1fc1c08089316232780408a131a5d83
Josh Oakes <joakes@ithemes.com>
2023-09-07 12:24:04 -0500

    KAD-1377 Allow users to disable custom browser validation

--- includes/advanced-form/advanced-form-cpt.php
+++ includes/advanced-form/advanced-form-cpt.php
@@ -525,6 +525,17 @@ class Kadence_Blocks_Form_CPT_Controller {
 
 		register_post_meta(
 			'kadence_form',
+			'_kad_form_browserValidation',
+			array(
+				'show_in_rest'  => true,
+				'single'        => true,
+				'auth_callback' => array( $this, 'meta_auth_callback' ),
+				'type'          => 'boolean',
+				'default'       => true
+			)
+		);
+		register_post_meta(
+			'kadence_form',
 			'_kad_form_redirect',
 			array(
 				'show_in_rest'  => true,
--- includes/blocks/class-kadence-blocks-advanced-form-block.php
+++ includes/blocks/class-kadence-blocks-advanced-form-block.php
@@ -412,6 +412,9 @@ class Kadence_Blocks_Advanced_Form_Block extends Kadence_Blocks_Abstract_Block {
 			'class' => implode( ' ', $inner_classes ),
 			'method' => 'post',
 		);
+		if( isset( $form_attributes['browserValidation'] ) && !$form_attributes['browserValidation'] ){
+			$inner_args['novalidate'] = 'true';
+		}
 		$inner_wrap_attributes = array();
 		foreach ( $inner_args as $key => $value ) {
 			$inner_wrap_attributes[] = $key . '="' . esc_attr( $value ) . '"';
