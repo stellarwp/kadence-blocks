311f0687b9959a8aad94b52ee87c00574c32f22c
Justin Frydman <justin.frydman@gmail.com>
2023-11-03 16:22:05 -0600

    Update provider to null log, filter batch size

--- includes/vendor/Image_Downloader/Image_Downloader_Provider.php
+++ includes/vendor/Image_Downloader/Image_Downloader_Provider.php
@@ -2,11 +2,14 @@
 
 namespace KadenceWP\KadenceBlocks\Image_Downloader;
 
+use KadenceWP\KadenceBlocks\Monolog\Handler\NullHandler;
+use KadenceWP\KadenceBlocks\Monolog\Logger;
+use KadenceWP\KadenceBlocks\Psr\Log\LoggerInterface;
 use KadenceWP\KadenceBlocks\StellarWP\ProphecyMonorepo\Container\Contracts\Provider;
 use KadenceWP\KadenceBlocks\StellarWP\ProphecyMonorepo\ImageDownloader\FileNameProcessor;
+use KadenceWP\KadenceBlocks\StellarWP\ProphecyMonorepo\ImageDownloader\ImageDownloader;
 use KadenceWP\KadenceBlocks\Symfony\Component\HttpClient\HttpClient;
 use KadenceWP\KadenceBlocks\Symfony\Contracts\HttpClient\HttpClientInterface;
-use StellarWP\ProphecyMonorepo\ImageDownloader\ImageDownloader;
 
 final class Image_Downloader_Provider extends Provider {
 
@@ -14,6 +17,17 @@ final class Image_Downloader_Provider extends Provider {
 	 * @inheritDoc
 	 */
 	public function register(): void {
+		// Disable any logging.
+		$this->container->bind( LoggerInterface::class, static function () {
+			$logger = new Logger( 'null' );
+			$logger->pushHandler( new NullHandler() );
+
+			return $logger;
+		} );
+
+		// Ensure we always get the same instance, so the image state is current.
+		$this->container->singleton( WordPress_Importer::class, WordPress_Importer::class );
+
 		$this->container->when( FileNameProcessor::class )
 		                ->needs( '$allowed_extensions' )
 		                ->give( [
@@ -27,10 +41,11 @@ final class Image_Downloader_Provider extends Provider {
 		                ->needs( HttpClientInterface::class )
 		                ->give( HttpClient::create() );
 
-		$batch_size = absint( apply_filters( 'kadence_blocks_image_download_batch_size', 20 ) );
+		$batch_size = absint( apply_filters( 'kadence_blocks_image_download_batch_size', 60 ) );
 
 		$this->container->when( ImageDownloader::class )
 		                ->needs( '$batch_size' )
 		                ->give( $batch_size );
 	}
+
 }
