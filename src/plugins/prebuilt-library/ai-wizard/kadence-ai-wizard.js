865000d48f5b2b8e9b0a590d181674e92cf232cf
Tanner Record <tanner.record@gmail.com>
2023-12-08 13:57:42 -0500

    Only send AI Wizard Completed event when complete

--- src/plugins/prebuilt-library/ai-wizard/kadence-ai-wizard.js
+++ src/plugins/prebuilt-library/ai-wizard/kadence-ai-wizard.js
@@ -77,6 +77,7 @@ export function KadenceAiWizard( props ) {
 
 	const { state, dispatch } = useKadenceAi();
 	const {
+		isComplete,
 		firstTime,
 		isSubmitted,
 		currentPageIndex,
@@ -95,12 +96,26 @@ export function KadenceAiWizard( props ) {
 		const saveStatus = await saveAiWizardData({
 			firstTime: false,
 			isSubmitted: true,
+			isComplete: isComplete,
 			...rest
 		});
 
 		if (saveStatus) {
 			onWizardClose( 'saved' );
 			handleEvent( 'ai_wizard_update' );
+		}
+	}
+
+	async function handleComplete() {
+		const complete = await saveAiWizardData({
+			firstTime: false,
+			isSubmitted: true,
+			isComplete: true,
+			...rest
+		});
+
+		if (complete) {
+			handleEvent('ai_wizard_complete');
 		}
 	}
 
@@ -120,7 +135,7 @@ export function KadenceAiWizard( props ) {
 		// Submit wizard data on finish buttton click.
 		if (event.type === 'click' && event.target.classList.contains('components-wizard__primary-button')) {
 			handleSave();
-			handleEvent( 'ai_wizard_complete' );
+			handleComplete();
 
 			if ( ! photographyOnly ) {
 				onPrimaryAction(event, true);
