d1365c28e806139feacc96c8ab1b876bd39c7211
Lalo Sanchez <eduardourias@stellarwp.com>
2024-02-09 18:15:47 -0600

    Make icon dissappear in the frontend when icon size is 0

--- includes/class-kadence-blocks-css.php
+++ includes/class-kadence-blocks-css.php
@@ -1736,19 +1736,19 @@ class Kadence_Blocks_CSS {
 		$unit = ! empty( $attributes[ $unit ] ) ? $attributes[ $unit ] : 'px';
 
 		$this->set_media_state( 'desktop' );
-		if ( ! empty( $attributes[ $name[0] ] ) ) {
+		if ( isset( $attributes[ $name[0] ] ) && '' !== $attributes[ $name[0] ] ) {
 			$this->add_property( $property, $attributes[ $name[0] ] . $unit );
 		} else if ( $defaults[0] ) {
 			$this->add_property( $property, $defaults[0] . $unit );
 		}
 		$this->set_media_state( 'tablet' );
-		if ( ! empty( $attributes[ $name[1] ] ) ) {
+		if ( isset( $attributes[ $name[0] ] ) && '' !== $attributes[ $name[0] ] ) {
 			$this->add_property( $property, $attributes[ $name[1] ] . $unit );
 		} else if ( $defaults[1] ) {
 			$this->add_property( $property, $defaults[1] . $unit );
 		}
 		$this->set_media_state( 'mobile' );
-		if ( ! empty( $attributes[ $name[2] ] ) ) {
+		if ( isset( $attributes[ $name[2] ] ) && '' !== $attributes[ $name[2] ] ) {
 			$this->add_property( $property, $attributes[ $name[2] ] . $unit );
 		} else if ( $defaults[2] ) {
 			$this->add_property( $property, $defaults[2] . $unit );
