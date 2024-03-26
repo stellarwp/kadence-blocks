7f6e8f319b109e9d80883bcfa4e58da2514fc3fb
Lalo Sanchez <eduardourias@stellarwp.com>
2024-03-26 17:12:18 -0600

    get nested lists from core to icon

--- src/blocks/iconlist/transforms.js
+++ src/blocks/iconlist/transforms.js
@@ -13,15 +13,27 @@ import { LINE_SEPARATOR, __UNSTABLE_LINE_SEPARATOR } from '@wordpress/rich-text'
 
 const lineSep = LINE_SEPARATOR ? LINE_SEPARATOR : __UNSTABLE_LINE_SEPARATOR;
 
+const getNestedListsFrom = (rawItems, level = 0) => {
+	const listItems = rawItems.flatMap((listItem) => {
+		if(undefined !== listItem.innerBlocks?.[0]?.name && listItem.innerBlocks[0].name === 'core/list' && listItem.innerBlocks[0].innerBlocks.length > 0) {
+			level++;
+			const nestedItems = getNestedListsFrom(listItem.innerBlocks[0].innerBlocks, level);
+			level--;
+			return [createBlock('kadence/listitem', { text: listItem.attributes.content, level: level }), ...nestedItems];
+		} else {
+			return createBlock('kadence/listitem', { text: listItem.attributes.content, level });
+		}
+	});
+	return listItems;
+}
+
 const transforms = {
 	from: [
 		{
 			type: 'block',
 			blocks: ['core/list'],
 			transform: (attributes, innerBlocks) => {
-				const listItems = innerBlocks.map((listItem) => {
-					return createBlock('kadence/listitem', { text: listItem.attributes.content });
-				});
+				const listItems = getNestedListsFrom(innerBlocks);
 
 				return createBlock(
 					'kadence/iconlist',
@@ -43,6 +55,7 @@ const transforms = {
 			type: 'block',
 			blocks: ['core/list'],
 			transform: (blockAttributes, innerBlocks) => {
+				console.log(innerBlocks);
 				const listItems = innerBlocks.map((listItem) => {
 					return createBlock('core/list-item', { content: listItem.attributes.text });
 				});
