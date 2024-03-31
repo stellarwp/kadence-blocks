039f57e8efa49c0431f976245d34d57569b401c3
Lalo Sanchez <eduardourias@stellarwp.com>
2024-03-31 16:38:48 -0600

    Fix variable error

--- includes/blocks/class-kadence-blocks-navigation-link-block.php
+++ includes/blocks/class-kadence-blocks-navigation-link-block.php
@@ -247,7 +247,7 @@ class Kadence_Blocks_Navigation_Link_Block extends Kadence_Blocks_Abstract_Block
 
 		$wrapper_classes[] = $is_mega_menu ? 'kadence-menu-mega-enabled' : '';
 		$wrapper_classes[] = $is_mega_menu ? $mega_menu_width_class : '';
-		$wrapper_classes[] = $nav_link_attributes['description'] ? 'kadence-menu-has-description' : '';
+		$wrapper_classes[] = ! empty($nav_link_attributes['description']) && $nav_link_attributes['description'] ? 'kadence-menu-has-description' : '';
 		$wrapper_classes[] = $has_icon ? 'kadence-menu-has-icon' : '';
 
 		$name = !empty( $nav_link_attributes['name'] ) ? $nav_link_attributes['name'] : '';
@@ -286,7 +286,7 @@ class Kadence_Blocks_Navigation_Link_Block extends Kadence_Blocks_Abstract_Block
 		}
 		$icon  = ! empty( $svg_icon ) ? '<div class="link-media-container"><span class="link-svg-icon link-svg-icon-' . esc_attr( $nav_link_attributes['mediaIcon'][0]['icon'] ) . '">' . $svg_icon . '</span></div>' : '';
 
-		$description = $nav_link_attributes['description'] ? '<span class="menu-label-description">' . $nav_link_attributes['description'] . '</span>' : '';
+		$description = ! empty($nav_link_attributes['description']) ? '<span class="menu-label-description">' . $nav_link_attributes['description'] . '</span>' : '';
 
 		return sprintf(
 			'<li %1$s><div class="link-drop-wrap"><a class="wp-block-kadence-navigation-link__content" href="' . esc_url( $url ) . '"><span class="link-drop-title-wrap">' . esc_html( $label ) . $icon . $description . '<span class="title-dropdown-navigation-toggle">%2$s</span></span></a></div>%3$s</li>',
