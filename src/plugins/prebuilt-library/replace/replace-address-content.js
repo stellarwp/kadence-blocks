e4700092fce6c07d4491e3008ae829cc81235606
Ben Ritner <ben@kadencewp.com>
2024-06-10 15:36:59 -0600

    tweak replace

--- src/plugins/prebuilt-library/replace/replace-address-content.js
+++ src/plugins/prebuilt-library/replace/replace-address-content.js
@@ -16,15 +16,15 @@ export default function replaceAddressContent(content, address) {
 	Country`,
 		address
 	);
-	content = content.replace(/1234 N Street<br>City, State, Country/g, address);
-	content = content.replace(/1234 N Street <br>City, State, Country/g, address);
-	content = content.replace(/1234 N Street <br>City, State<br>Country/g, address);
-	content = content.replace(/1234 N Street <br>City, State <br>Country/g, address);
-	content = content.replace(/1234 N Street City, State, Country/g, address);
+	content = content.replaceAll('1234 N Street<br>City, State, Country', address);
+	content = content.replaceAll('1234 N Street <br>City, State, Country', address);
+	content = content.replaceAll('1234 N Street <br>City, State<br>Country', address);
+	content = content.replaceAll('1234 N Street <br>City, State <br>Country', address);
+	content = content.replaceAll('1234 N Street City, State, Country', address);
 	content = content.replace('1234 N Street <br/>City, State, Country', address);
-	content = content.replace(/Los angeles/g, address);
-	content = content.replace(/Las angeles/g, address);
-	content = content.replace(/Los Angeles/g, address);
-	content = content.replace(/Las Angeles/g, address);
+	content = content.replaceAll('Los angeles', address);
+	content = content.replaceAll('Las angeles', address);
+	content = content.replaceAll('Los Angeles', address);
+	content = content.replaceAll('Las Angeles', address);
 	return content;
 }
