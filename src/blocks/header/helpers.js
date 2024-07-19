09e3c98c2e3e237279fac9b1eca1524c52605c0e
Josh Oakes <josh@joshoak.es>
2024-07-19 15:11:17 -0500

    Fix placeholder URL for view in iFrame

--- src/blocks/header/helpers.js
+++ src/blocks/header/helpers.js
@@ -249,5 +249,37 @@ function getDataFromKey(key) {
 		response.templateInnerBlocks = MultiRow5InnerBlocks();
 	}
 
+	// Replace placeholder relative URL with absolute URL
+	if (response.templateInnerBlocks) {
+		response.templateInnerBlocks = replaceInAttributes(
+			response.templateInnerBlocks,
+			'/wp-content/plugins/kadence-blocks/includes/assets/images/placeholder/',
+			kadence_blocks_params.kadenceBlocksUrl + '/includes/assets/images/placeholder/'
+		);
+	}
+
 	return response;
+}
+
+function replaceInAttributes(obj, searchValue, replaceValue) {
+	// Check if the input is an array
+	if (Array.isArray(obj)) {
+		// Loop through each item in the array
+		obj.forEach((item, index) => {
+			obj[index] = replaceInAttributes(item, searchValue, replaceValue);
+		});
+	} else if (typeof obj === 'object' && obj !== null) {
+		// Loop through each key in the object
+		for (const key in obj) {
+			if (typeof obj[key] === 'string') {
+				// If the value is a string, replace the substring
+				obj[key] = obj[key].replace(searchValue, replaceValue);
+			} else {
+				// If the value is an object or array, call the function recursively
+				obj[key] = replaceInAttributes(obj[key], searchValue, replaceValue);
+			}
+		}
+	}
+	// Return the modified object
+	return obj;
 }
