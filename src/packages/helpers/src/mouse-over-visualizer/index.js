1663e994efc4fe9e55eaf7fe6189a20137977ddb
Josh Oakes <josh@joshoak.es>
2024-08-14 12:48:36 -0500

    KAD-3233 Use helpers repository in blocks free

--- src/packages/helpers/src/mouse-over-visualizer/index.js
+++
@@ -1,8 +0,0 @@
-import { useState } from '@wordpress/element';
-
-export default function mouseOverVisualizer() {
-	const [ isMouseOver, setIsMouseOver ] = useState( false );
-	const onMouseOver = () => setIsMouseOver( true );
-	const onMouseOut = () => setIsMouseOver( false );
-	return { isMouseOver, onMouseOver, onMouseOut };
-}
