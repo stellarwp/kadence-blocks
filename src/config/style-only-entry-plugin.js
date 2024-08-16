d75a06c5d7a3807aacb8e7431a6bdc280271e9ca
Josh Oakes <josh@joshoak.es>
2024-08-15 22:49:36 -0500

    Refactor out deprecations

--- src/config/style-only-entry-plugin.js
+++ src/config/style-only-entry-plugin.js
@@ -1,41 +1,45 @@
-function StyleOnlyEntryPlugin(styleTests) {
-	let list = [];
-
-	if (styleTests instanceof RegExp) {
-		list.push(styleTests);
-	} else if (Array.isArray(styleTests) && styleTests.length) {
-		list = styleTests;
-	} else {
-		list = [/\.s?css$/];
-	}
-
-	Object.assign(this, {
-		styleTests: list,
-	});
-}
-
-StyleOnlyEntryPlugin.prototype.isFileStyle = function (file) {
-	for (const test of this.styleTests) {
-		if (test.test(file)) {
-			return true;
+class StyleOnlyEntryPlugin {
+	constructor(styleTests) {
+		if (styleTests instanceof RegExp) {
+			this.styleTests = [styleTests];
+		} else if (Array.isArray(styleTests) && styleTests.length) {
+			this.styleTests = styleTests;
+		} else {
+			this.styleTests = [/\.s?css$/];
 		}
 	}
 
-	return false;
-};
+	isFileStyle(file) {
+		// Use some() for early exit if a match is found
+		return this.styleTests.some((test) => test.test(file));
+	}
 
-StyleOnlyEntryPlugin.prototype.apply = function (compiler) {
-	compiler.hooks.emit.tap('style-only-entry-plugin', (compilation) => {
-		for (const chunk of compilation.chunks) {
-			if (chunk.entryModule && this.isFileStyle(chunk.entryModule.userRequest)) {
-				for (const file of chunk.files) {
-					if (!this.isFileStyle(file)) {
-						delete compilation.assets[file];
+	apply(compiler) {
+		compiler.hooks.emit.tap('StyleOnlyEntryPlugin', (compilation) => {
+			const chunkGraph = compilation.chunkGraph;
+
+			for (const chunk of compilation.chunks) {
+				// Use the ChunkGraph API to get the entry modules for this chunk
+				const entryModules = chunkGraph.getChunkEntryModulesIterable(chunk);
+
+				for (const module of entryModules) {
+					if (this.isFileStyle(module.userRequest)) {
+						// Collect all style files in a Set for quick lookup
+						const styleFiles = new Set(chunk.files.filter((file) => this.isFileStyle(file)));
+
+						// Only delete non-style assets
+						for (const file of chunk.files) {
+							if (!styleFiles.has(file)) {
+								delete compilation.assets[file];
+							}
+						}
+						// Break after processing the first style entry module
+						break;
 					}
 				}
 			}
-		}
-	});
-};
+		});
+	}
+}
 
 module.exports = StyleOnlyEntryPlugin;
