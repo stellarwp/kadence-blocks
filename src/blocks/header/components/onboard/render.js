50ccdb674e3cefff895274e67f225fa28f33aa26
Ben Ritner <ben@kadencewp.com>
2024-10-02 17:28:51 -0600

    Fix for trans header in preview

--- src/blocks/header/components/onboard/render.js
+++ src/blocks/header/components/onboard/render.js
@@ -7,9 +7,8 @@ const HeaderRender = ({ id }) => {
 	const [meta] = useEntityProp('postType', 'kadence_header', 'meta', id);
 
 	const desktopBlocks = get(blocks, [0, 'innerBlocks', 0], []);
-
 	// Give transparent headers a background
-	const additionalStyles = meta?._kad_header_isTransparent
+	const additionalStyles = meta?._kad_header_isTransparent && meta?._kad_header_isTransparent === '1'
 		? [
 				{
 					css: '.kadence-header-row-inner { background: linear-gradient(45deg, #ccc 25%, transparent 25%), linear-gradient(135deg, #ccc 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #ccc 75%), linear-gradient(135deg, transparent 75%, #ccc 75%); background-size: 25px 25px; background-position: 0 0, 12.5px 0, 12.5px -12.5px, 0px 12.5px; }',
