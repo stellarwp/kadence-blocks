72eb39bdf45bf2cfc9cfd84a50638b622ac83955
Josh Oakes <josh@joshoak.es>
2024-10-29 14:02:18 -0500

    KAD-3711 Auto select the SVG you've just uploaded

--- src/packages/components/src/icon-picker/index.js
+++ src/packages/components/src/icon-picker/index.js
@@ -85,7 +85,8 @@ export default function KadenceIconPicker({
 		setDeleteId( null );
 	};
 
-	const addCallback = () => {
+	const addCallback = ( postId ) => {
+		onChange('kb-custom-' + postId.toString() );
 		getCustomSvgs( true );
 	};
 
