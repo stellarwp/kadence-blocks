c9f729140e08921feafb2c8b19684beef87728d5
Mark Woodard <mark.c.woodard@gmail.com>
2024-11-25 00:30:04 -0600

    Fixing an issue with middle row shrink height responsive control https://stellarwp.atlassian.net/browse/KAD-3664

--- src/blocks/header/edit-inner.js
+++ src/blocks/header/edit-inner.js
@@ -666,13 +666,14 @@ export function EditInner(props) {
 										checked={shrinkMain}
 										onChange={(value) => setMetaAttribute(value, 'shrinkMain')}
 									/>
+									{console.log(1, shrinkMainHeight, shrinkMainHeightTablet, shrinkMainHeightMobile)}
 									{shrinkMain &&
 										(previewStickySection.includes('main') || previewStickySection === '') && (
 											<ResponsiveRangeControls
 												label={__('Middle Row Shrink Height', 'kadence-blocks')}
 												value={parseFloat(shrinkMainHeight)}
-												valueTablet={parseFloat(shrinkMainHeightTablet)}
-												valueMobile={parseFloat(shrinkMainHeightMobile)}
+												tabletValue={parseFloat(shrinkMainHeightTablet)}
+												mobileValue={parseFloat(shrinkMainHeightMobile)}
 												onChange={(value) =>
 													setMetaAttribute(value.toString(), 'shrinkMainHeight')
 												}
