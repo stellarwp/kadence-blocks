0f862aeabc4103447f6ecf946b41dbb26e88a614
Lint Action <lint-action@samuelmeuli.com>
2025-03-21 15:55:08 +0000

    Fix code style issues with ESLint

--- src/plugins/settings.js
+++ src/plugins/settings.js
@@ -24,39 +24,42 @@ function KadenceSetting(props) {
 		config[key] = value;
 		const settingModel = new wp.api.models.Settings({ kadence_blocks_settings: JSON.stringify(config) });
 
-		settingModel.save().then((response) => {
-			createSuccessNotice(__('Settings saved', 'kadence-blocks'), {
-				type: 'snackbar',
-			});
-
-			setIsSaving(false);
-			setSettings(config);
-			kadence_blocks_params.globalSettings = JSON.stringify(config);
-			successCallback && successCallback(key, value);
-		}).then(() => {
-			const { getBlocks } = wp.data.select('core/block-editor');
-			const allBlocks = getBlocks();
-			
-			// If block has uniqueId, trigger rerender without marking as changed
-			const processBlocks = (blocks) => {
-				blocks.forEach((block) => {
-					if (block.attributes && block.attributes.uniqueID) {
-						const { updateBlockAttributes } = wp.data.dispatch('core/block-editor');
-						// Dont pass next change to undo stack
-						wp.data.dispatch('core/block-editor').__unstableMarkNextChangeAsNotPersistent();
-						updateBlockAttributes(block.clientId, { 
-							'data-kb-rerender': Date.now()
-						});
-					}
-					
-					if (block.innerBlocks && block.innerBlocks.length) {
-						processBlocks(block.innerBlocks);
-					}
+		settingModel
+			.save()
+			.then((response) => {
+				createSuccessNotice(__('Settings saved', 'kadence-blocks'), {
+					type: 'snackbar',
 				});
-			};
-			
-			processBlocks(allBlocks);
-		});
+
+				setIsSaving(false);
+				setSettings(config);
+				kadence_blocks_params.globalSettings = JSON.stringify(config);
+				successCallback && successCallback(key, value);
+			})
+			.then(() => {
+				const { getBlocks } = wp.data.select('core/block-editor');
+				const allBlocks = getBlocks();
+
+				// If block has uniqueId, trigger rerender without marking as changed
+				const processBlocks = (blocks) => {
+					blocks.forEach((block) => {
+						if (block.attributes && block.attributes.uniqueID) {
+							const { updateBlockAttributes } = wp.data.dispatch('core/block-editor');
+							// Dont pass next change to undo stack
+							wp.data.dispatch('core/block-editor').__unstableMarkNextChangeAsNotPersistent();
+							updateBlockAttributes(block.clientId, {
+								'data-kb-rerender': Date.now(),
+							});
+						}
+
+						if (block.innerBlocks && block.innerBlocks.length) {
+							processBlocks(block.innerBlocks);
+						}
+					});
+				};
+
+				processBlocks(allBlocks);
+			});
 	};
 
 	return (
