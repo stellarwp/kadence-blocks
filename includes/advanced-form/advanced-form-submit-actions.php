9bcba1af032c57dcbc37bf57ef661f6f0dbe8fe0
Josh Oakes <josh@joshoak.es>
2025-04-17 10:17:18 -0500

    KAD-4318 Single request to wp_mail regardless of recipients

--- includes/advanced-form/advanced-form-submit-actions.php
+++ includes/advanced-form/advanced-form-submit-actions.php
@@ -175,13 +175,18 @@ class Kadence_Blocks_Advanced_Form_Submit_Actions {
 			}
 			$cc_headers = 'Cc: ' . implode( ',', $sanitized_cc_emails ) . "\r\n";
 		}
-		$mail = wp_mail( $to, $subject, $body, $headers . $cc_headers );
+
+		$bcc_headers = '';
 		if ( $email_bcc ) {
 			$bcc_emails = explode( ',', $email_bcc );
+			$sanitized_bcc_emails = array();
 			foreach ( $bcc_emails as $bcc_email ) {
-				wp_mail( sanitize_email( trim( $bcc_email ) ), $subject, $body, $headers );
+				$sanitized_bcc_emails[] = sanitize_email( trim( $bcc_email ) );
 			}
+			$bcc_headers = 'Bcc: ' . implode( ',', $sanitized_bcc_emails ) . "\r\n";
 		}
+		
+		wp_mail( $to, $subject, $body, $headers . $cc_headers . $bcc_headers );
 	}
 	/**
 	 * Mailerlite rest call.
