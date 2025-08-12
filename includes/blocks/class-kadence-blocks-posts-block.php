80095fad3174854b4d32897ba9ccfb3f62e10973
Ben Ritner <ben@kadencewp.com>
2025-08-11 23:35:18 -0600

    update post lists

--- includes/blocks/class-kadence-blocks-posts-block.php
+++ includes/blocks/class-kadence-blocks-posts-block.php
@@ -52,16 +52,16 @@ class Kadence_Blocks_Posts_Block extends Kadence_Blocks_Abstract_Block {
 	/**
 	 * Builds CSS for block.
 	 *
-	 * @param array $attributes the blocks attributes.
+	 * @param array              $attributes the blocks attributes.
 	 * @param Kadence_Blocks_CSS $css the css class for blocks.
-	 * @param string $unique_id the blocks attr ID.
-	 * @param string $unique_style_id the blocks alternate ID for queries.
+	 * @param string             $unique_id the blocks attr ID.
+	 * @param string             $unique_style_id the blocks alternate ID for queries.
 	 */
 	public function build_css( $attributes, $css, $unique_id, $unique_style_id ) {
 
 		$css->set_style_id( 'kb-' . $this->block_name . $unique_style_id );
 
-		$title_font = ( isset( $attributes ) && is_array( $attributes ) && isset( $attributes['titleFont'] ) && is_array( $attributes['titleFont'] ) && isset( $attributes['titleFont'][0] ) && is_array( $attributes['titleFont'][0] ) ? $attributes['titleFont'][0] : array() );
+		$title_font = ( isset( $attributes ) && is_array( $attributes ) && isset( $attributes['titleFont'] ) && is_array( $attributes['titleFont'] ) && isset( $attributes['titleFont'][0] ) && is_array( $attributes['titleFont'][0] ) ? $attributes['titleFont'][0] : [] );
 		$css->set_selector( '.kb-posts-id-' . $unique_id . ' .entry.loop-entry .entry-header .entry-title' );
 		if ( isset( $title_font['size'] ) && is_array( $title_font['size'] ) && isset( $title_font['size'][0] ) && ! empty( $title_font['size'][0] ) ) {
 			$css->add_property( 'font-size', $css->get_font_size( $title_font['size'][0], ( ! isset( $title_font['sizeType'] ) ? 'px' : $title_font['sizeType'] ) ) );
@@ -85,12 +85,10 @@ class Kadence_Blocks_Posts_Block extends Kadence_Blocks_Abstract_Block {
 				$css->set_selector( '.kb-posts-id-' . $unique_id . ' .loop-entry .post-thumbnail' );
 				$css->add_property( 'margin-bottom', '1em' );
 			}
-		} else {
-			if ( class_exists( 'Kadence\Theme' ) && defined( 'KADENCE_VERSION' ) ) {
-				if ( version_compare( KADENCE_VERSION, '1.0.16', '<' ) ) {
-					$css->set_selector( '.wp-block-kadence-posts.kb-posts-id-' . $unique_id . ' .entry.loop-entry > .entry-content-wrap' );
-					$css->add_property( 'padding', '2rem' );
-				}
+		} elseif ( class_exists( 'Kadence\Theme' ) && defined( 'KADENCE_VERSION' ) ) {
+			if ( version_compare( KADENCE_VERSION, '1.0.16', '<' ) ) {
+				$css->set_selector( '.wp-block-kadence-posts.kb-posts-id-' . $unique_id . ' .entry.loop-entry > .entry-content-wrap' );
+				$css->add_property( 'padding', '2rem' );
 			}
 		}
 		$css->set_media_state( 'tablet' );
@@ -138,16 +136,17 @@ class Kadence_Blocks_Posts_Block extends Kadence_Blocks_Abstract_Block {
 		}
 		global $kadence_blocks_posts_not_in;
 		if ( ! isset( $kadence_blocks_posts_not_in ) || ! is_array( $kadence_blocks_posts_not_in ) ) {
-			$kadence_blocks_posts_not_in = array();
+			$kadence_blocks_posts_not_in = [];
 		}
 		// CSS classes.
-		$classes   = array();
+		$classes   = [];
 		$classes[] = 'wp-block-kadence-posts';
 		$classes[] = 'kb-posts';
-		$classes[] = 'kb-posts-id-' . ( isset( $attributes['uniqueID'] ) ? $attributes['uniqueID'] : '' );
+		$classes[] = 'kadence-posts-list';
+		$classes[] = 'kb-posts-id-' . ( $attributes['uniqueID'] ?? '' );
 		$classes[] = 'content-wrap';
 		$classes[] = 'grid-cols';
-		$classes[] = 'kb-posts-style-' . ( isset( $attributes['loopStyle'] ) ? $attributes['loopStyle'] : 'boxed' );
+		$classes[] = 'kb-posts-style-' . ( $attributes['loopStyle'] ?? 'boxed' );
 		if ( isset( $attributes['columns'] ) && ! empty( $attributes['columns'] ) ) {
 			$columns = absint( $attributes['columns'] );
 		} else {
@@ -201,45 +200,45 @@ class Kadence_Blocks_Posts_Block extends Kadence_Blocks_Abstract_Block {
 			}
 		}
 		$post_type = ( isset( $attributes['postType'] ) && ! empty( $attributes['postType'] ) ? $attributes['postType'] : 'post' );
-		$args      = array(
+		$args      = [
 			'post_type'           => $post_type,
 			'posts_per_page'      => ( isset( $attributes['postsToShow'] ) && ! empty( $attributes['postsToShow'] ) ? $attributes['postsToShow'] : 6 ),
 			'post_status'         => 'publish',
 			'order'               => ( isset( $attributes['order'] ) && ! empty( $attributes['order'] ) ? $attributes['order'] : 'desc' ),
 			'orderby'             => ( isset( $attributes['orderBy'] ) && ! empty( $attributes['orderBy'] ) ? $attributes['orderBy'] : 'date' ),
 			'ignore_sticky_posts' => ( isset( $attributes['allowSticky'] ) && $attributes['allowSticky'] ? 0 : 1 ),
-			'post__not_in'        => ( isset( $kadence_blocks_posts_not_in ) && is_array( $kadence_blocks_posts_not_in ) ? $kadence_blocks_posts_not_in : array() ),
-		);
+			'post__not_in'        => ( isset( $kadence_blocks_posts_not_in ) && is_array( $kadence_blocks_posts_not_in ) ? $kadence_blocks_posts_not_in : [] ),
+		];
 		if ( isset( $attributes['offsetQuery'] ) && ! empty( $attributes['offsetQuery'] ) ) {
 			$args['offset'] = $attributes['offsetQuery'];
 		}
 		if ( isset( $attributes['categories'] ) && ! empty( $attributes['categories'] ) && is_array( $attributes['categories'] ) ) {
-			$categories = array();
+			$categories = [];
 			$i          = 1;
 			foreach ( $attributes['categories'] as $key => $value ) {
 				$categories[] = $value['value'];
 			}
 		} else {
-			$categories = array();
+			$categories = [];
 		}
 		if ( 'post' !== $post_type || ( isset( $attributes['postTax'] ) && true === $attributes['postTax'] ) ) {
 			if ( isset( $attributes['taxType'] ) && ! empty( $attributes['taxType'] ) ) {
-				$args['tax_query'][] = array(
+				$args['tax_query'][] = [
 					'taxonomy' => ( isset( $attributes['taxType'] ) ) ? $attributes['taxType'] : 'category',
 					'field'    => 'id',
 					'terms'    => $categories,
 					'operator' => ( isset( $attributes['excludeTax'] ) && 'exclude' === $attributes['excludeTax'] ? 'NOT IN' : 'IN' ),
-				);
+				];
 			}
 		} else {
 			if ( isset( $attributes['tags'] ) && ! empty( $attributes['tags'] ) && is_array( $attributes['tags'] ) ) {
-				$tags = array();
+				$tags = [];
 				$i    = 1;
 				foreach ( $attributes['tags'] as $key => $value ) {
 					$tags[] = $value['value'];
 				}
 			} else {
-				$tags = array();
+				$tags = [];
 			}
 			if ( isset( $attributes['excludeTax'] ) && 'exclude' === $attributes['excludeTax'] ) {
 				$args['category__not_in'] = $categories;
@@ -255,15 +254,15 @@ class Kadence_Blocks_Posts_Block extends Kadence_Blocks_Abstract_Block {
 
 		do_action( 'kadence_blocks_posts_query_start', $attributes, $loop );
 		if ( $loop->have_posts() ) {
-			echo '<div class="' . esc_attr( esc_attr( implode( ' ', $classes ) ) ) . '">';
+			echo '<ul class="' . esc_attr( esc_attr( implode( ' ', $classes ) ) ) . '">';
 			while ( $loop->have_posts() ) {
 				$loop->the_post();
 				if ( isset( $attributes['showUnique'] ) && true === $attributes['showUnique'] ) {
 					$kadence_blocks_posts_not_in[] = get_the_ID();
 				}
-				kadence_blocks_get_template( 'entry.php', array( 'attributes' => $attributes ) );
+				kadence_blocks_get_template( 'entry.php', [ 'attributes' => $attributes ] );
 			}
-			echo '</div>';
+			echo '</ul>';
 		} else {
 			echo wp_kses_post( apply_filters( 'kadence_blocks_posts_empty_query', '<p>' . esc_html__( 'No posts', 'kadence-blocks' ) . '</p>' ) );
 		}
@@ -274,7 +273,6 @@ class Kadence_Blocks_Posts_Block extends Kadence_Blocks_Abstract_Block {
 		ob_end_clean();
 
 		return $output . $content;
-
 	}
 	/**
 	 * Render for block scripts block.
@@ -283,7 +281,14 @@ class Kadence_Blocks_Posts_Block extends Kadence_Blocks_Abstract_Block {
 	 * @param boolean $inline true or false based on when called.
 	 */
 	public function render_scripts( $attributes, $inline = false ) {
-		if ( ! class_exists( 'Kadence\Theme' ) || apply_filters( 'kadence_blocks_post_block_style_force_output', false ) ) {
+		// Don't need to enqueue styles for this block if Kadence theme is active and it's newer than 1.3.0
+		$has_kadence_theme = class_exists( 'Kadence\Theme' );
+		if ( $has_kadence_theme && defined( 'KADENCE_VERSION' ) && version_compare( KADENCE_VERSION, '1.3.0', '>=' ) ) {
+			$has_kadence_theme = true;
+		} else {
+			$has_kadence_theme = false;
+		}
+		if ( ! $has_kadence_theme || apply_filters( 'kadence_blocks_post_block_style_force_output', false ) ) {
 			if ( ! wp_style_is( 'kadence-blocks-' . $this->block_name, 'enqueued' ) ) {
 				$this->enqueue_style( 'kadence-blocks-' . $this->block_name );
 				if ( $inline ) {
@@ -292,7 +297,6 @@ class Kadence_Blocks_Posts_Block extends Kadence_Blocks_Abstract_Block {
 			}
 		}
 	}
-
 }
 
 Kadence_Blocks_Posts_Block::get_instance();
--- includes/templates/entry.php
+++ includes/templates/entry.php
@@ -10,17 +10,19 @@
 defined( 'ABSPATH' ) || exit;
 $enabled_image = ( isset( $attributes ) && is_array( $attributes ) && isset( $attributes['image'] ) && ! $attributes['image'] ? false : true );
 ?>
-<article <?php post_class( 'entry content-bg loop-entry' . ( ! $enabled_image ? ' kb-post-no-image' : '' ) ); ?>>
-	<?php
-		kadence_blocks_get_template( 'entry-loop-thumbnail.php', array( 'attributes' => $attributes ) );
-	?>
-	<div class="entry-content-wrap">
+<li class="kb-post-list-item">
+	<article <?php post_class( 'entry content-bg loop-entry' . ( ! $enabled_image ? ' kb-post-no-image' : '' ) ); ?>>
 		<?php
-		kadence_blocks_get_template( 'entry-loop-header.php', array( 'attributes' => $attributes ) );
-
-		kadence_blocks_get_template( 'entry-summary.php', array( 'attributes' => $attributes ) );
-
-		kadence_blocks_get_template( 'entry-loop-footer.php', array( 'attributes' => $attributes ) );
+			kadence_blocks_get_template( 'entry-loop-thumbnail.php', [ 'attributes' => $attributes ] );
 		?>
-	</div>
-</article>
+		<div class="entry-content-wrap">
+			<?php
+			kadence_blocks_get_template( 'entry-loop-header.php', [ 'attributes' => $attributes ] );
+
+			kadence_blocks_get_template( 'entry-summary.php', [ 'attributes' => $attributes ] );
+
+			kadence_blocks_get_template( 'entry-loop-footer.php', [ 'attributes' => $attributes ] );
+			?>
+		</div>
+	</article>
+</li>
