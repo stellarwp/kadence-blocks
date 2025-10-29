44b87109025892c601f220d6292f134eb815c958
gilbert-hernandez <gilbert.hernandez@kadencewp.com>
2025-10-29 09:50:19 -0600

    KAD-5170 Generate CSS variable from backgroundColorClass when it is a palette color.

--- src/blocks/advancedheading/edit.js
+++ src/blocks/advancedheading/edit.js
@@ -1006,10 +1006,11 @@ function KadenceAdvancedHeading(props) {
 				gap: icon ? '0.25em' : undefined,
 				justifyContent: icon && previewJustifyAlign ? previewJustifyAlign : undefined,
 				textAlign: previewAlign ? previewAlign : undefined,
-				backgroundColor:
-					!enableTextGradient && background && (backgroundIgnoreClass || isPaletteColor)
-						? KadenceColorOutput(background)
-						: undefined,
+				backgroundColor: !enableTextGradient && (background && backgroundIgnoreClass || isPaletteColor) 
+					? (isPaletteColor && backgroundColorClass 
+						? 'var(--global-' + backgroundColorClass.replace('kb-', '').replace('theme-', '') + ')' 
+						: KadenceColorOutput(background))
+					: undefined,
 				backgroundImage: enableTextGradient && textGradient !== '' ? textGradient : undefined,
 				paddingTop:
 					'' !== previewPaddingTop ? getSpacingOptionOutput(previewPaddingTop, paddingType) : undefined,
