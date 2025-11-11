b2fe12a383efdde52b5d96cfac401003433290fb
Justin Frydman <justin.frydman@gmail.com>
2025-11-11 15:04:38 -0700

    KAD-4362: Update State, add StateTest.php

--- includes/resources/Optimizer/State.php
+++ includes/resources/Optimizer/State.php
@@ -2,16 +2,38 @@
 
 namespace KadenceWP\KadenceBlocks\Optimizer;
 
-final class State {
+class State {
+
+	public const SETTINGS_KEY = 'performance_optimizer_enabled';
+
+	/**
+	 * The default state of the Optimizer if no
+	 * settings have been saved.
+	 */
+	public const DEFAULT_STATE = false;
 
 	/**
 	 * Whether the Optimizer is enabled.
 	 *
-	 * @TODO hook this up to a setting.
-	 *
 	 * @return bool
 	 */
 	public function enabled(): bool {
-		return true;
+		$settings_json = (string) get_option( 'kadence_blocks_settings', '' );
+
+		if ( ! $settings_json ) {
+			return self::DEFAULT_STATE;
+		}
+
+		$settings = json_decode( $settings_json, true );
+
+		if ( ! is_array( $settings ) ) {
+			return self::DEFAULT_STATE;
+		}
+
+		return filter_var(
+			$settings[ self::SETTINGS_KEY ] ?? self::DEFAULT_STATE,
+			FILTER_VALIDATE_BOOL,
+			FILTER_NULL_ON_FAILURE
+		) ?? self::DEFAULT_STATE;
 	}
 }
