327221a21ac4ed459a642b50427ecc82d813cadf
Justin Frydman <justin.frydman@gmail.com>
2025-11-14 16:15:19 -0700

    KAD-4362: Refactor giant Optimizer_Provider into separate providers

---
+++ includes/resources/Optimizer/Status/Provider.php
@@ -0,0 +1,34 @@
+<?php declare( strict_types=1 );
+
+namespace KadenceWP\KadenceBlocks\Optimizer\Status;
+
+use KadenceWP\KadenceBlocks\Optimizer\Status\Meta;
+use KadenceWP\KadenceBlocks\StellarWP\ProphecyMonorepo\Container\Contracts\Provider as Provider_Contract;
+
+final class Provider extends Provider_Contract {
+
+	public function register(): void {
+		$this->container->singleton( Meta::class, Meta::class );
+
+		add_action(
+			'init',
+			$this->container->callback( Meta::class, 'register_meta' )
+		);
+
+		add_action(
+			'enqueue_block_editor_assets',
+			$this->container->callback( Meta::class, 'enqueue_meta_script' ),
+			15,
+			0
+		);
+
+		add_action(
+			'updated_post_meta',
+			function ( $meta_id, $post_id, $meta_key, $value ) {
+				$this->container->get( Meta::class )->maybe_clear_optimizer_data( $post_id, $meta_key, $value );
+			},
+			10,
+			4
+		);
+	}
+}
