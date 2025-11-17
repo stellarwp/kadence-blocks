362e2ad44fa063a4f9f2008eb20b6c24884dc0dc
Justin Frydman <justin.frydman@gmail.com>
2025-11-17 15:28:24 -0700

    KAD-4362: Make plugin pinnable, add view link, properly reset optimizer status state

--- includes/resources/Optimizer/assets/js/meta/constants.js
+++ includes/resources/Optimizer/assets/js/meta/constants.js
@@ -6,3 +6,4 @@ export const META_KEY = '_kb_optimizer_status';
 // An excluded post is stored in post meta as -1.
 export const STATUS_EXCLUDED = -1;
 export const STATUS_UNOPTIMIZED = 0;
+export const STATUS_OPTIMIZED = 1;
--- includes/resources/Optimizer/assets/js/meta/meta.js
+++ includes/resources/Optimizer/assets/js/meta/meta.js
@@ -1,9 +1,11 @@
+import { useEffect, useRef } from '@wordpress/element';
 import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/editor';
 import { __ } from '@wordpress/i18n';
 import { ToggleControl, PanelBody } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { kadenceNewIcon } from '@kadence/icons';
 import { NAME, META_KEY, STATUS_EXCLUDED, STATUS_UNOPTIMIZED } from './constants';
+import OptimizedViewLink from '../optimized-view-link';
 
 /**
  * Adds a Gutenberg plugin to exclude the current post from being optimized.
@@ -15,6 +17,14 @@ import { NAME, META_KEY, STATUS_EXCLUDED, STATUS_UNOPTIMIZED } from './constants
 const OptimizerExcludeMeta = () => {
 	const meta = useSelect((select) => select('core/editor').getEditedPostAttribute('meta'));
 	const { editPost } = useDispatch('core/editor');
+	const originalMetaValue = useRef(null);
+
+	// Capture the original meta value when it first becomes available.
+	useEffect(() => {
+		if (meta !== undefined && originalMetaValue.current === null && meta[META_KEY] !== undefined) {
+			originalMetaValue.current = meta[META_KEY];
+		}
+	}, [meta]);
 
 	if (meta === undefined) {
 		return null;
@@ -26,19 +36,23 @@ const OptimizerExcludeMeta = () => {
 				{__('Kadence Performance Optimizer', 'kadence-blocks')}
 			</PluginSidebarMoreMenuItem>
 			<PluginSidebar
-				isPinnable={false}
+				isPinnable={true}
 				icon={kadenceNewIcon}
 				name={NAME}
-				title={__('Optimizer Settings For This Post', 'kadence-blocks')}
+				title={__('Optimizer Settings', 'kadence-blocks')}
 			>
 				<PanelBody>
 					<ToggleControl
-						label={__('Exclude from optimization', 'kadence-blocks')}
+						label={__('Exclude this post from optimization', 'kadence-blocks')}
 						checked={meta[META_KEY] === STATUS_EXCLUDED}
-						onChange={(value) =>
-							editPost({ meta: { [META_KEY]: value ? STATUS_EXCLUDED : STATUS_UNOPTIMIZED } })
-						}
+						onChange={(value) => {
+							const fallbackValue =
+								originalMetaValue.current !== null ? originalMetaValue.current : STATUS_UNOPTIMIZED;
+							editPost({ meta: { [META_KEY]: value ? STATUS_EXCLUDED : fallbackValue } });
+						}}
 					/>
+
+					<OptimizedViewLink />
 				</PanelBody>
 			</PluginSidebar>
 		</>
