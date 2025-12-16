1b7bd04bb62e01515cbff33b3771c58232cb710a
pramodjodhani <mrpramodjodhani@gmail.com>
2025-12-16 13:36:39 +0530

    fix problem where the mobile collapse order doesnt work when the Collapse Order is Reverse

--- includes/blocks/class-kadence-blocks-column-block.php
+++ includes/blocks/class-kadence-blocks-column-block.php
@@ -743,7 +743,11 @@ class Kadence_Blocks_Column_Block extends Kadence_Blocks_Abstract_Block {
 			$css->add_property( 'margin-right', 'auto' );
 		}
 		if ( isset( $attributes['collapseOrder'] ) ) {
-			$css->set_selector( '.kt-row-column-wrap.kt-mobile-layout-three-grid > .kadence-column' . $unique_id . ', .kt-row-column-wrap.kt-mobile-layout-two-grid > .kadence-column' . $unique_id . ', .kt-row-column-wrap.kt-mobile-layout-row > .kadence-column' . $unique_id );
+			$css->set_selector(
+				'.kt-row-column-wrap.kt-mobile-layout-three-grid > .kadence-column' . $unique_id . ',
+				.kt-row-column-wrap.kt-mobile-layout-two-grid > .kadence-column' . $unique_id . ',
+				.kb-row-layout-wrap.wp-block-kadence-rowlayout .kt-row-column-wrap.kt-mobile-layout-row > .kadence-column' . $unique_id
+			);
 			$css->add_property( 'order', $attributes['collapseOrder'] );
 		}
 		if ( ! empty( $attributes['textAlign'][2] ) ) {
