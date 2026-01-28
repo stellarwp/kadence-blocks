0ae4cf8f0a455b9ddb299af541cd8935f6050c90
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2026-01-28 18:02:44 +0530

    chore: migrate packages to external dependencies
    Remove local package implementations and add external references
    for @kadence/components, @kadence/helpers, and @kadence/icons

--- src/packages/icons/src/block-icons/countdown-block/section/index.js
+++
@@ -1,4 +0,0 @@
-export default <svg width="20px" height="20px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1.414">
-	<path d="M47.612,40.008l-19.999,0l0,-32.016l13.566,0l6.433,0.023l0,31.993Z" fill="var(--kadence-color, #0058b0 )" fillRule="nonzero" />
-	<path d="M20.383,40l-20.036,0l0,-32l20.036,0.019l0,31.981Z" fill="var(--kadence-color, #0058b0 )" fillRule="nonzero" />
-</svg>;
