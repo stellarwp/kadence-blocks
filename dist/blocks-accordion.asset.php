55b6462c4cde2bdd2beddfa41ddf6716806e0101
Jeremy Strom <jstrom@situationinteractive.com>
2026-02-22 11:59:07 -0600

    Build dist

--- dist/blocks-accordion.asset.php
+++ dist/blocks-accordion.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('kadence-components', 'kadence-helpers', 'kadence-icons', 'lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ead16f8868fd4a40fd62');
+<?php return array('dependencies' => array('kadence-components', 'kadence-helpers', 'kadence-icons', 'lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'de2a6bc6f84b43cd6725');
