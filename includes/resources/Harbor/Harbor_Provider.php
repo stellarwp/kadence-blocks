286babd8169e9fa1735465fbc83ea86ef9cc2e03
Jon Waldstein <Jpwaldstein@gmail.com>
2026-04-02 17:42:31 -0400

    feature: add uplink filter based notices

--- includes/resources/Harbor/Harbor_Provider.php
+++ includes/resources/Harbor/Harbor_Provider.php
@@ -20,6 +20,51 @@ final class Harbor_Provider extends Provider {
 
 		lw_harbor_register_submenu( 'kadence-blocks' );
 		add_filter( 'lw-harbor/legacy_licenses', [ $this, 'report_legacy_licenses' ] );
+
+		$extensions = [
+			'kadence-blocks'   => __( 'Kadence Blocks', 'kadence-blocks' ),
+			'kadence-insights' => __( 'Kadence Insights', 'kadence-blocks' ),
+		];
+
+		foreach ( $extensions as $hook_prefix => $product_name ) {
+			add_action(
+				"stellarwp/uplink/{$hook_prefix}/license_field_after_form",
+				function() use ( $product_name ) {
+					$this->render_harbor_license_notice( $product_name );
+				}
+			);
+		}
+
+		if ( class_exists( 'KadenceWP\KadenceBlocksPro\Uplink\Connect' ) ) {
+			remove_action( 'admin_notices', [ \KadenceWP\KadenceBlocksPro\Uplink\Connect::get_instance(), 'inactive_notice' ] );
+		}
+	}
+
+	/**
+	 * Renders a Harbor notice below the save button on a Kadence Uplink license field.
+	 *
+	 * @param string $product_name The human-readable product name.
+	 *
+	 * @return void
+	 */
+	public function render_harbor_license_notice( string $product_name ): void {
+		$url = lw_harbor_get_license_page_url();
+		if ( empty( $url ) ) {
+			return;
+		}
+		?>
+		<hr style="margin: 20px 0;"/>
+		<h4><span class="dashicons dashicons-info" style="vertical-align: middle; margin-right: 4px;"></span><?php esc_html_e( 'Liquid Web Software Manager', 'kadence-blocks' ); ?></h4>
+		<p class="tooltip description"><?php echo wp_kses(
+			sprintf(
+				/* translators: 1: product name, 2: URL to the Liquid Web Software Manager page. */
+				__( '%1$s is now part of Liquid Web\'s software offerings. This field is still available for managing legacy licenses from your previous %1$s account. If you purchased a new plan through Liquid Web, your products are managed through the <a href="%2$s">Liquid Web Software Manager</a>.', 'kadence-blocks' ),
+				esc_html( $product_name ),
+				esc_url( $url )
+			),
+			[ 'a' => [ 'href' => [] ] ]
+		); ?></p>
+		<?php
 	}
 
 	/**
