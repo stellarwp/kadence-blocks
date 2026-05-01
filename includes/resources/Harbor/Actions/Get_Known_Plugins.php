e9f6a21ffe14c1a51917413f06e963f29ea6f553
Jon Waldstein <Jpwaldstein@gmail.com>
2026-05-01 14:22:14 -0400

    fix: wp error, no need to translate product names

--- includes/resources/Harbor/Actions/Get_Known_Plugins.php
+++ includes/resources/Harbor/Actions/Get_Known_Plugins.php
@@ -17,47 +17,47 @@ final class Get_Known_Plugins {
 
 		return [
 			'kadence-blocks'       => [
-				'name'     => __( 'Kadence Blocks', 'kadence-blocks' ),
+				'name'     => 'Kadence Blocks',
 				'page_url' => $default_page,
 			],
 			'kadence-blocks-pro'   => [
-				'name'     => __( 'Kadence Blocks Pro', 'kadence-blocks' ),
+				'name'     => 'Kadence Blocks Pro',
 				'page_url' => $default_page,
 			],
 			'kadence-creative-kit' => [
-				'name'     => __( 'Kadence Creative Kit', 'kadence-blocks' ),
+				'name'     => 'Kadence Creative Kit',
 				'page_url' => admin_url( 'admin.php?page=kadence-blocks-home&license=show' ),
 			],
 			'kadence-insights'     => [
-				'name'     => __( 'Kadence Insights', 'kadence-blocks' ),
+				'name'     => 'Kadence Insights',
 				'page_url' => admin_url( 'admin.php?page=kadence-insights-settings&license=show' ),
 			],
 			'kadence-shop-kit'     => [
-				'name'     => __( 'Kadence Shop Kit', 'kadence-blocks' ),
+				'name'     => 'Kadence Shop Kit',
 				'page_url' => admin_url( 'admin.php?page=kadence-shop-kit-settings&license=show' ),
 			],
 			'kadence-galleries'    => [
-				'name'     => __( 'Kadence Galleries', 'kadence-blocks' ),
+				'name'     => 'Kadence Galleries',
 				'page_url' => $default_page,
 			],
 			'kadence-conversions'  => [
-				'name'     => __( 'Kadence Conversions', 'kadence-blocks' ),
+				'name'     => 'Kadence Conversions',
 				'page_url' => $default_page,
 			],
 			'kadence-captcha'      => [
-				'name'     => __( 'Kadence Captcha', 'kadence-blocks' ),
+				'name'     => 'Kadence Captcha',
 				'page_url' => admin_url( 'admin.php?page=kadence-recaptcha-settings&license=show' ),
 			],
 			'kadence-theme-pro'    => [
-				'name'     => __( 'Kadence Theme Pro', 'kadence-blocks' ),
+				'name'     => 'Kadence Theme Pro',
 				'page_url' => $default_page,
 			],
 			'kadence-pattern-hub'  => [
-				'name'     => __( 'Kadence Pattern Hub', 'kadence-blocks' ),
+				'name'     => 'Kadence Pattern Hub',
 				'page_url' => $default_page,
 			],
 			'kadence-white-label'  => [
-				'name'     => __( 'Kadence White Label', 'kadence-blocks' ),
+				'name'     => 'Kadence White Label',
 				'page_url' => admin_url( 'admin.php?page=kadence-white-label-settings' ),
 			],
 		];
