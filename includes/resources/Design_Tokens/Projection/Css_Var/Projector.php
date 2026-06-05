a5724af870afce8f65952f7f962daf16ff44e0ac
Paulo Iankoski <me@wpdev.com.br>
2026-06-05 15:57:30 -0300

    Add `Projection/Contracts/Projector` marker interface; `Css_Var\\Projector` implements it

--- includes/resources/Design_Tokens/Projection/Css_Var/Projector.php
+++ includes/resources/Design_Tokens/Projection/Css_Var/Projector.php
@@ -2,6 +2,7 @@
 
 namespace KadenceWP\KadenceBlocks\Design_Tokens\Projection\Css_Var;
 
+use KadenceWP\KadenceBlocks\Design_Tokens\Projection\Contracts\Projector as Projector_Contract;
 use KadenceWP\KadenceBlocks\Design_Tokens\Registry\Token_Definition;
 use KadenceWP\KadenceBlocks\Design_Tokens\Registry\Token_Registry;
 use KadenceWP\KadenceBlocks\Design_Tokens\Resolver\Resolved_Tokens;
@@ -26,7 +27,7 @@ use KadenceWP\KadenceBlocks\Design_Tokens\Resolver\Resolved_Tokens;
  *
  * @since TBD
  */
-final class Projector {
+final class Projector implements Projector_Contract {
 
 	/**
 	 * The root scope for both declaration blocks. :where() contributes zero specificity, so neither
