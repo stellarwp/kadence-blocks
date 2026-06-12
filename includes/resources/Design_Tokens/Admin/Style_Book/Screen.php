13f8496bac2f9f01e8024c15bd3c6d1b477ba4b8
Paulo Iankoski <me@wpdev.com.br>
2026-06-12 13:50:36 -0300

    Add Style Book admin screen PHP classes
    Register the Kadence submenu item, render the React mount point, and
    enqueue the bundle using wp-scripts asset metadata for dependencies
    and cache busting.

---
+++ includes/resources/Design_Tokens/Admin/Style_Book/Screen.php
@@ -0,0 +1,51 @@
+<?php declare( strict_types=1 );
+
+namespace KadenceWP\KadenceBlocks\Design_Tokens\Admin\Style_Book;
+
+/**
+ * Renders the Style Book admin screen mount point.
+ *
+ * @since TBD
+ */
+final class Screen {
+
+	/**
+	 * The DOM id the React bundle mounts into.
+	 *
+	 * @since TBD
+	 *
+	 * @var string
+	 */
+	private const ROOT_ID = 'kadence-style-book-root';
+
+	/**
+	 * The DOM id the React bundle mounts into.
+	 *
+	 * @since TBD
+	 *
+	 * @return string
+	 */
+	public static function get_root_id(): string {
+		return self::ROOT_ID;
+	}
+
+	/**
+	 * Output the Style Book screen markup.
+	 *
+	 * @since TBD
+	 *
+	 * @return void
+	 */
+	public function render(): void {
+		do_action( 'stellarwp/telemetry/kadence-blocks/optin' );
+		?>
+		<div class="wrap kadence_blocks_dash kadence-style-book-wrap">
+			<div class="kadence_blocks_dash_head_container">
+				<div class="kadence_blocks_dash_wrap">
+					<div id="<?php echo esc_attr( self::ROOT_ID ); ?>" class="kadence-style-book-root"></div>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+}
