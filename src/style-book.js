33a6e0c21c0b4377a55ded3ae7e1fa786114973c
Lint Action <lint-action@samuelmeuli.com>
2026-06-12 17:49:16 +0000

    Fix code style issues with ESLint

--- src/style-book.js
+++ src/style-book.js
@@ -5,12 +5,12 @@ import { createRoot } from '@wordpress/element';
 import { StyleBookApp } from './style-book/app/StyleBookApp';
 import './style-book/style-book.scss';
 
-wp.domReady( () => {
-	const container = document.getElementById( 'kadence-style-book-root' );
+wp.domReady(() => {
+	const container = document.getElementById('kadence-style-book-root');
 
-	if ( ! container ) {
+	if (!container) {
 		return;
 	}
 
-	createRoot( container ).render( <StyleBookApp /> );
-} );
+	createRoot(container).render(<StyleBookApp />);
+});
--- src/style-book/app/StyleBookApp.js
+++ src/style-book/app/StyleBookApp.js
@@ -11,9 +11,9 @@ import { __ } from '@wordpress/i18n';
 export function StyleBookApp() {
 	return (
 		<div className="kadence-style-book">
-			<h1 className="kadence-style-book__title">{ __( 'Style Book', 'kadence-blocks' ) }</h1>
+			<h1 className="kadence-style-book__title">{__('Style Book', 'kadence-blocks')}</h1>
 			<p className="kadence-style-book__intro">
-				{ __( 'Preview and manage Kadence design tokens from one place.', 'kadence-blocks' ) }
+				{__('Preview and manage Kadence design tokens from one place.', 'kadence-blocks')}
 			</p>
 		</div>
 	);
