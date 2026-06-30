91c381d48d881732c709e5c969a32610dd659bd7
Paulo Iankoski <me@wpdev.com.br>
2026-06-30 14:52:57 -0300

    refactor(style-book): rename root CSS class to kadence-blocks-style-book

--- src/style-book/app/StyleBookApp.js
+++ src/style-book/app/StyleBookApp.js
@@ -10,9 +10,9 @@ import { __ } from '@wordpress/i18n';
  */
 export function StyleBookApp() {
 	return (
-		<div className="kadence-style-book">
-			<h1 className="kadence-style-book__title">{__('Style Book', 'kadence-blocks')}</h1>
-			<p className="kadence-style-book__intro">
+		<div className="kadence-blocks-style-book">
+			<h1 className="kadence-blocks-style-book__title">{__('Style Book', 'kadence-blocks')}</h1>
+			<p className="kadence-blocks-style-book__intro">
 				{__('Preview and manage Kadence design tokens from one place.', 'kadence-blocks')}
 			</p>
 		</div>
