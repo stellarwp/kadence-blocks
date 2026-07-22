57433013da8e83a19b8be1406f2abcf421f37fc1
Rahula Palu Caleffi <rahula_dd@hotmail.com>
2026-07-22 14:47:56 -0300

    fix: improve block attribute output handling

--- tests/wpunit/Blocks/ProgressBarTest.php
+++ tests/wpunit/Blocks/ProgressBarTest.php
@@ -25,5 +25,45 @@ class ProgressBarTest extends KadenceBlocksUnit {
 		$this->block = new Kadence_Blocks_Progress_Bar_Block();
 	}
 
+	/**
+	 * Render the block from its serialized attributes.
+	 *
+	 * @param string $attributes JSON encoded block attributes.
+	 *
+	 * @return string
+	 */
+	private function render( string $attributes ): string {
+		return do_blocks( '<!-- wp:kadence/progress-bar ' . $attributes . ' /-->' );
+	}
 
+	public function testUniqueIdIsNormalizedInMarkupAndStyles() {
+		$html = $this->render( '{"uniqueID":"1_aaa\u0022 onmouseover=\u0022alert(1)"}' );
+
+		$this->assertStringNotContainsString( 'onmouseover="', $html );
+		$this->assertStringContainsString( 'id="current-progress-above1_aaaonmouseoveralert1"', $html );
+		$this->assertStringContainsString( 'kb-progress-bar-container1_aaaonmouseoveralert1', $html );
+	}
+
+	public function testNumberPrefixAndSuffixAreEscaped() {
+		$html = $this->render( '{"uniqueID":"2_bbb","numberPrefix":"<img src=x onerror=alert(1)>","numberSuffix":"</span><script>alert(2)</script>"}' );
+
+		$this->assertStringNotContainsString( '<img', $html );
+		$this->assertStringNotContainsString( '<script>', $html );
+		$this->assertStringContainsString( '&lt;img src=x onerror=alert(1)&gt;', $html );
+	}
+
+	public function testAlignIsEscapedInContainerClasses() {
+		$html = $this->render( '{"uniqueID":"3_ccc","align":"none\u0022 onfocus=\u0022alert(1)"}' );
+
+		$this->assertStringNotContainsString( 'onfocus="', $html );
+		$this->assertStringContainsString( 'alignnone&quot; onfocus=&quot;alert(1)', $html );
+	}
+
+	public function testValidAttributesRenderUnchanged() {
+		$html = $this->render( '{"uniqueID":"372_bebd31-3e","numberPrefix":"$","numberSuffix":" USD","progressAmount":42,"showMaxProgressOnPageLoad":true}' );
+
+		$this->assertStringContainsString( 'kb-progress-bar-container372_bebd31-3e', $html );
+		$this->assertStringContainsString( 'id="current-progress-above372_bebd31-3e"', $html );
+		$this->assertStringContainsString( '>$42 USD</span>', $html );
+	}
 }
