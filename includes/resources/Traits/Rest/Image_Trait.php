0aa7f981b2b37299a80f849bbfdfff897783f4f5
WayneRocha <dev.waynerocha@gmail.com>
2026-08-10 21:03:05 -0300

    feat: add array validation to prevent DoS attacks and improve image size sanitization

--- includes/resources/Traits/Rest/Image_Trait.php
+++ includes/resources/Traits/Rest/Image_Trait.php
@@ -19,23 +19,24 @@ trait Image_Trait {
 	 * @return array|array<int, array{id: int, width: int, height: int, crop: bool}>
 	 */
 	public function sanitize_image_sizes_array( $sizes ): array {
-		$new_sizes = [];
-
 		if ( ! is_array( $sizes ) ) {
-			return $new_sizes;
+			return array();
 		}
+
+		$sizes = array_slice( $sizes, 0, 50 );
+		$new_sizes = array();
 
 		foreach ( $sizes as $value ) {
 			if ( ! is_array( $value ) ) {
 				continue;
 			}
 
-			$new_sizes[] = [
-				'id'     => sanitize_text_field( $value['id'] ),
-				'width'  => absint( $value['width'] ),
-				'height' => absint( $value['height'] ),
-				'crop'   => filter_var( $value['crop'], FILTER_VALIDATE_BOOLEAN ),
-			];
+			$new_sizes[] = array(
+				'id'     => isset( $value['id'] ) ? sanitize_text_field( $value['id'] ) : '',
+				'width'  => isset( $value['width'] ) ? absint( $value['width'] ) : 0,
+				'height' => isset( $value['height'] ) ? absint( $value['height'] ) : 0,
+				'crop'   => isset( $value['crop'] ) ? filter_var( $value['crop'], FILTER_VALIDATE_BOOLEAN ) : false,
+			);
 		}
 
 		return $new_sizes;
