ead56f24222328d3d1e307f34f7f2b185ed8a26b
Paulo Iankoski <me@wpdev.com.br>
2026-08-16 12:27:32 -0300

    fix(advancedgallery): cast the image width before escaping it

--- includes/blocks/class-kadence-blocks-advancedgallery-block.php
+++ includes/blocks/class-kadence-blocks-advancedgallery-block.php
@@ -12,6 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+use KadenceWP\KadenceBlocks\Utils\Cast;
+
 /**
  * Class to Build the Advanced Gallery Block.
  *
@@ -1163,7 +1165,7 @@ class Kadence_Blocks_Advancedgallery_Block extends Kadence_Blocks_Abstract_Block
 		$output .= '<div class="kadence-blocks-gallery-item-inner">';
 		$figure_style = '';
 		if ( ! empty( $padding_bottom ) && 'below' === $caption_style ) {
-			$figure_style = ' style="max-width:' . esc_attr( $image['width'] ) . 'px;"';
+			$figure_style = ' style="max-width:' . esc_attr( Cast::to_string( $image['width'] ) ) . 'px;"';
 		}
 		$output .= '<figure class="' . esc_attr( implode( ' ', $fig_classes ) ) . '"' . $figure_style . '>';
 		if ( ! empty( $href ) ) {
